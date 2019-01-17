7b0325c61f46430308ce438906173d4d27aca304
Vladimir Voronkov <VSVoronkov@gmail.com>
2019-01-17 15:47:17 +0200

    change getCandles method of Responder

--- responder/index.js
+++ responder/index.js
@@ -1,24 +1,51 @@
 'use strict'
 
+const { pick } = require('lodash')
+
+const schema = require('./schema')
+const { getMethodLimit } = require('./helpers')
+
 class Responder {
   constructor (wrk, core) {
     this.wrk = wrk
     this.core = core
     this.reportService = wrk.ctx.api_bfx
+    this.logger = wrk.logger
   }
 
   // TODO:
-  async getCandles (args, cb) {
+  async getCandles (methodName = 'getCandles', args, cb) {
     try {
-      const params = args.params || {}
+      this.core.checkParams(
+        args,
+        'paramsSchemaForCandlesApi',
+        ['timeframe', 'symbol', 'section'],
+        schema
+      )
+
+      const params = args.params && typeof args.params === 'object'
+        ? args.params
+        : {}
+      params.end = this.core.getDateNotMoreNow(params.end)
+      params.limit = getMethodLimit(
+        this.core.getMethodLimit,
+        params.limit,
+        'candles'
+      )
+      const query = pick(params, [
+        'limit',
+        'start',
+        'end',
+        'sort'
+      ])
       const rest = this.core.getREST(args.auth, this.wrk)
 
-      const data = await rest.candles.bind(rest)(params)
+      const data = await rest.candles.bind(rest)({ ...params, query })
 
       const res = this.core.prepareResponse(
         data,
         'mts',
-        500,
+        params.limit,
         params.notThrowError,
         params.notCheckNextPage
       )
@@ -26,7 +53,7 @@ class Responder {
       if (!cb) return res
       cb(null, res)
     } catch (err) {
-      this._err(err, '_getCandles', cb)
+      this.core.logError(this.logger, err, methodName, cb)
     }
   }
 }
