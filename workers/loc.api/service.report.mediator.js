ea59e4172068827a529da5f51b05fb052d518f79
Vladimir Voronkov <VSVoronkov@gmail.com>
2019-02-19 12:33:41 +0200

    implement api _getCandles method of MediatorReportService

--- workers/loc.api/service.report.mediator.js
+++ workers/loc.api/service.report.mediator.js
@@ -1,7 +1,64 @@
 'use strict'
 
-const BaseMediatorReportService = require('bfx-report/workers/loc.api/service.report.mediator')
+const { pick } = require('lodash')
 
-class MediatorReportService extends BaseMediatorReportService {}
+const BaseMediatorReportService = require(
+  'bfx-report/workers/loc.api/service.report.mediator'
+)
+const {
+  getREST,
+  getDateNotMoreNow,
+  prepareResponse
+} = require('bfx-report/workers/loc.api/helpers')
+
+const {
+  checkParams,
+  getMethodLimit
+} = require('./helpers')
+
+class MediatorReportService extends BaseMediatorReportService {
+  async _getCandles (args) {
+    try {
+      checkParams(
+        args,
+        'paramsSchemaForCandlesApi',
+        ['timeframe', 'symbol', 'section']
+      )
+
+      const params = (
+        args.params &&
+        typeof args.params === 'object'
+      )
+        ? args.params
+        : {}
+      params.end = getDateNotMoreNow(params.end)
+      params.limit = getMethodLimit(
+        params.limit,
+        'candles'
+      )
+      const query = pick(params, [
+        'limit',
+        'start',
+        'end',
+        'sort'
+      ])
+      const rest = getREST({}, this.ctx.grc_bfx.caller)
+
+      const data = await rest.candles.bind(rest)({ ...params, query })
+
+      const res = prepareResponse(
+        data,
+        'mts',
+        params.limit,
+        params.notThrowError,
+        params.notCheckNextPage
+      )
+
+      return res
+    } catch (err) {
+      this._err(err, '_getCandles')
+    }
+  }
+}
 
 module.exports = MediatorReportService
