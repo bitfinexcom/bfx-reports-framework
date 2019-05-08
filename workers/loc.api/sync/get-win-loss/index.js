7f61160ff6fd715f7a7a8aba7d8a7d75321d1cb4
Vladimir Voronkov <VSVoronkov@gmail.com>
2019-05-08 16:20:41 +0300

    add movements calculation to win/loss report

--- workers/loc.api/sync/get-win-loss/index.js
+++ workers/loc.api/sync/get-win-loss/index.js
@@ -1,16 +1,58 @@
 'use strict'
 
 const {
-  calcGroupedData
+  getInsertableArrayObjectsFilter
+} = require('bfx-report/workers/loc.api/sync/dao/helpers')
+
+const ALLOWED_COLLS = require('../allowed.colls')
+const {
+  getModelsMap,
+  getMethodCollMap
+} = require('../schema')
+const {
+  calcGroupedData,
+  groupByTimeframe,
+  isForexSymb
 } = require('../helpers')
 const getBalanceHistory = require('../get-balance-history')
 
-// TODO: need to implement calc of DEPOSIT/WITHDRAWALS
+const _sumMovementsWithPrevRes = (
+  symbol,
+  prevMovementsRes,
+  movementsGroupedByTimeframe
+) => {
+  return symbol.reduce((accum, symb) => {
+    const prevMovement = Number.isFinite(prevMovementsRes[symb])
+      ? prevMovementsRes[symb]
+      : 0
+    const movement = Number.isFinite(movementsGroupedByTimeframe[symb])
+      ? movementsGroupedByTimeframe[symb]
+      : 0
+    const res = prevMovement + movement
+
+    return {
+      ...accum,
+      [symb]: res
+    }
+  }, {})
+}
+
 const _getWinLossByTimeframe = (
   symbol = [],
   startWalletsVals = {}
 ) => {
-  return ({ walletsGroupedByTimeframe } = {}) => {
+  let prevMovementsRes = {}
+
+  return ({
+    walletsGroupedByTimeframe,
+    movementsGroupedByTimeframe
+  } = {}) => {
+    prevMovementsRes = _sumMovementsWithPrevRes(
+      symbol,
+      prevMovementsRes,
+      { ...movementsGroupedByTimeframe }
+    )
+
     return symbol.reduce((accum, symb) => {
       const startWallet = Number.isFinite(startWalletsVals[symb])
         ? startWalletsVals[symb]
@@ -18,7 +60,10 @@ const _getWinLossByTimeframe = (
       const wallet = Number.isFinite(walletsGroupedByTimeframe[symb])
         ? walletsGroupedByTimeframe[symb]
         : 0
-      const res = wallet - startWallet
+      const movements = Number.isFinite(prevMovementsRes[symb])
+        ? prevMovementsRes[symb]
+        : 0
+      const res = (wallet - startWallet) + movements
 
       return {
         ...accum,
@@ -28,8 +73,37 @@ const _getWinLossByTimeframe = (
   }
 }
 
+const _calcMovements = (
+  data = [],
+  symbolFieldName,
+  symbol = []
+) => {
+  return data.reduce((accum, movement = {}) => {
+    const { amount, amountUsd } = { ...movement }
+    const currSymb = movement[symbolFieldName]
+    const _isForexSymb = isForexSymb(symbol, currSymb)
+    const _amount = _isForexSymb
+      ? amount
+      : amountUsd
+    const symb = _isForexSymb
+      ? currSymb
+      : 'USD'
+
+    if (!Number.isFinite(_amount)) {
+      return { ...accum }
+    }
+
+    return {
+      ...accum,
+      [symb]: (Number.isFinite(accum[symb]))
+        ? accum[symb] + _amount
+        : _amount
+    }
+  }, {})
+}
+
 module.exports = async (
-  { dao },
+  rService,
   {
     auth = {},
     params: {
@@ -39,6 +113,9 @@ module.exports = async (
     } = {}
   } = {}
 ) => {
+  const { dao } = rService
+  const user = await rService.dao.checkAuthInDb({ auth })
+
   const symbol = ['EUR', 'JPY', 'GBP', 'USD']
   const args = {
     auth,
@@ -48,6 +125,42 @@ module.exports = async (
       end
     }
   }
+  const movementsModel = getModelsMap().get(ALLOWED_COLLS.MOVEMENTS)
+  const movementsMethodColl = getMethodCollMap().get('_getMovements')
+  const {
+    dateFieldName: movementsDateFieldName,
+    symbolFieldName: movementsSymbolFieldName
+  } = movementsMethodColl
+
+  const movementsBaseFilter = getInsertableArrayObjectsFilter(
+    movementsMethodColl,
+    {
+      start,
+      end
+    }
+  )
+
+  const movements = await dao.getElemsInCollBy(
+    ALLOWED_COLLS.MOVEMENTS,
+    {
+      filter: {
+        ...movementsBaseFilter,
+        user_id: user._id
+      },
+      sort: [['mtsUpdated', -1]],
+      projection: movementsModel,
+      exclude: ['user_id'],
+      isExcludePrivate: true
+    }
+  )
+  const movementsGroupedByTimeframe = await groupByTimeframe(
+    movements,
+    timeframe,
+    symbol,
+    movementsDateFieldName,
+    movementsSymbolFieldName,
+    _calcMovements
+  )
 
   const walletsGroupedByTimeframe = await getBalanceHistory(
     { dao },
@@ -64,7 +177,8 @@ module.exports = async (
 
   const res = await calcGroupedData(
     {
-      walletsGroupedByTimeframe
+      walletsGroupedByTimeframe,
+      movementsGroupedByTimeframe
     },
     false,
     _getWinLossByTimeframe(symbol, startWalletsInForex),
