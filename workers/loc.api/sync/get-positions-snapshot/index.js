0180b72bac988ce2089948e575444ed015ecf7a6
Vladimir Voronkov <VSVoronkov@gmail.com>
2019-06-06 12:32:32 +0300

    improve getting positions

--- workers/loc.api/sync/get-positions-snapshot/index.js
+++ workers/loc.api/sync/get-positions-snapshot/index.js
@@ -3,6 +3,96 @@
 const ALLOWED_COLLS = require('../allowed.colls')
 const { getModelsMap } = require('../schema')
 
+const _getPositionsHistory = (
+  dao,
+  user,
+  endMts,
+  startMts
+) => {
+  const positionsHistoryModel = getModelsMap()
+    .get(ALLOWED_COLLS.POSITIONS_HISTORY)
+
+  return dao.getElemsInCollBy(
+    ALLOWED_COLLS.POSITIONS_HISTORY,
+    {
+      filter: {
+        user_id: user._id,
+        $lte: { mtsCreate: endMts },
+        $gte: { mtsUpdate: startMts }
+      },
+      sort: [['mtsUpdate', -1]],
+      projection: positionsHistoryModel,
+      exclude: ['user_id'],
+      isExcludePrivate: true
+    }
+  )
+}
+
+const _getClosedPosition = (
+  positionsHistory,
+  posAudit
+) => {
+  const { id } = { ...posAudit }
+
+  const posHist = positionsHistory.find(({ status, id: posHistId }) => (
+    status === 'CLOSED' &&
+    id === posHistId
+  ))
+
+  return posHist || posAudit
+}
+
+const _filterPositions = (
+  positionsAudit,
+  positionsHistory,
+  year,
+  month,
+  day
+) => {
+  return positionsAudit.reduce(
+    (accum, posAudit) => {
+      const { mtsUpdate, status } = { ...posAudit }
+
+      if (!Number.isInteger(mtsUpdate)) {
+        return accum
+      }
+
+      const date = new Date(mtsUpdate)
+
+      if (
+        year === date.getUTCFullYear() &&
+        month === date.getUTCMonth() &&
+        day === date.getUTCDate()
+      ) {
+        if (status === 'CLOSED') {
+          const closedPosition = _getClosedPosition(
+            positionsHistory,
+            posAudit
+          )
+
+          accum.push(closedPosition)
+
+          return accum
+        }
+
+        accum.push(posAudit)
+      }
+
+      return accum
+    }, [])
+}
+
+const _getPositionsHistoryIds = (positionsHistory) => {
+  return positionsHistory.reduce(
+    (accum, { id } = {}) => {
+      if (Number.isInteger(id)) {
+        accum.push(id)
+      }
+
+      return accum
+    }, [])
+}
+
 // TODO:
 module.exports = async (
   rService,
@@ -17,33 +107,17 @@ module.exports = async (
   const user = await rService.dao.checkAuthInDb({ auth })
 
   const date = new Date(end)
-  const startMts = Date.UTC(
-    date.getUTCFullYear(),
-    date.getUTCMonth(),
-    date.getUTCDate()
-  )
-  const endMts = Date.UTC(
-    date.getUTCFullYear(),
-    date.getUTCMonth(),
-    date.getUTCDate() + 1
-  ) - 1
+  const year = date.getUTCFullYear()
+  const month = date.getUTCMonth()
+  const day = date.getUTCDate()
+  const startMts = Date.UTC(year, month, day)
+  const endMts = Date.UTC(year, month, day + 1) - 1
 
-  const positionsHistoryModel = getModelsMap()
-    .get(ALLOWED_COLLS.POSITIONS_HISTORY)
-
-  const positionsHistory = await dao.getElemsInCollBy(
-    ALLOWED_COLLS.POSITIONS_HISTORY,
-    {
-      filter: {
-        user_id: user._id,
-        $lte: { mtsCreate: endMts },
-        $gte: { mtsUpdate: startMts }
-      },
-      sort: [['mtsUpdate', -1]],
-      projection: positionsHistoryModel,
-      exclude: ['user_id'],
-      isExcludePrivate: true
-    }
+  const positionsHistory = await _getPositionsHistory(
+    dao,
+    user,
+    endMts,
+    startMts
   )
 
   if (
@@ -53,5 +127,26 @@ module.exports = async (
     return []
   }
 
-  return positionsHistory // TODO:
+  const ids = _getPositionsHistoryIds(positionsHistory)
+  const { res: positionsAudit } = await rService.getPositionsAudit(
+    null,
+    { auth, params: { id: [...ids] } }
+  )
+
+  if (
+    !Array.isArray(positionsAudit) ||
+    positionsAudit.length === 0
+  ) {
+    return []
+  }
+
+  const positions = _filterPositions(
+    positionsAudit,
+    positionsHistory,
+    year,
+    month,
+    day
+  )
+
+  return positions // TODO:
 }
