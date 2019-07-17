346ef81d7b43bce1acaa1dadd9062450360e6b8e
ZIMkaRU <vsvoronkov@gmail.com>
2019-07-17 20:41:36 +0300

    change DI configs

--- workers/loc.api/di/app.deps.js
+++ workers/loc.api/di/app.deps.js
@@ -1,12 +1,8 @@
 'use strict'
 
 const { ContainerModule } = require('inversify')
-
 const bindDepsToFn = require(
   'bfx-report/workers/loc.api/di/bind-deps-to-fn'
-)
-const bindDepsToInstance = require(
-  'bfx-report/workers/loc.api/di/bind-deps-to-instance'
 )
 
 const TYPES = require('./types')
@@ -14,7 +10,15 @@ const TYPES = require('./types')
 const ALLOWED_COLLS = require('../sync/allowed.colls')
 const WSTransport = require('../ws-transport')
 const WSEventEmitter = require('../ws-transport/ws.event.emitter')
-// const DataInserter = require('./loc.api/sync/data.inserter')
+const Progress = require('../sync/progress')
+const syncSchema = require('../sync/schema')
+const sync = require('../sync')
+const SyncQueue = require('../sync/sync.queue')
+const {
+  redirectRequestsToApi
+} = require('../sync/helpers')
+const DataInserter = require('../sync/data.inserter')
+const SqliteDAO = require('../sync/dao/dao.sqlite')
 
 module.exports = ({
   grcBfxOpts
@@ -27,7 +31,11 @@ module.exports = ({
           ['_conf', TYPES.CONF],
           ['_sync', TYPES.Sync],
           ['_wsEventEmitter', TYPES.WSEventEmitter],
-          ['_ALLOWED_COLLS', TYPES.ALLOWED_COLLS]
+          ['_ALLOWED_COLLS', TYPES.ALLOWED_COLLS],
+          ['_prepareResponse', TYPES.PrepareResponse],
+          ['_progress', TYPES.Progress],
+          ['_syncSchema', TYPES.SyncSchema],
+          ['_dao', TYPES.DAO]
         ]
       })
       .inSingletonScope()
@@ -39,5 +47,71 @@ module.exports = ({
     bind(TYPES.WSEventEmitter)
       .to(WSEventEmitter)
       .inSingletonScope()
+    bind(TYPES.Progress)
+      .to(Progress)
+      .inSingletonScope()
+    bind(TYPES.SyncSchema).toConstantValue(
+      syncSchema
+    )
+    bind(TYPES.SqliteDAO)
+      .to(SqliteDAO)
+    bind(TYPES.DAO)
+      .toDynamicValue((ctx) => {
+        const { dbDriver } = ctx.container.get(
+          TYPES.CONF
+        )
+        const rService = ctx.container.get(
+          TYPES.RService
+        )
+
+        if (dbDriver === 'sqlite') {
+          const db = rService.ctx.dbSqlite_m0.db
+          const dao = ctx.container.get(
+            TYPES.SqliteDAO
+          )
+          dao.setDB(db)
+
+          return dao
+        }
+      })
+      .inSingletonScope()
+    bind(TYPES.DataInserter)
+      .to(DataInserter)
+    bind(TYPES.DataInserterFactory)
+      .toFactory((ctx) => {
+        return (syncColls) => {
+          const dataInserter = ctx.container.get(
+            TYPES.DataInserter
+          )
+          dataInserter.init(syncColls)
+
+          return dataInserter
+        }
+      })
+    bind(TYPES.SyncQueue)
+      .to(SyncQueue)
+      .inSingletonScope()
+    bind(TYPES.RedirectRequestsToApi).toConstantValue(
+      bindDepsToFn(
+        redirectRequestsToApi,
+        [
+          TYPES.DAO,
+          TYPES.WSEventEmitter
+        ],
+        true
+      )
+    )
+    bind(TYPES.Sync).toConstantValue(
+      bindDepsToFn(
+        sync,
+        [
+          TYPES.SyncQueue,
+          TYPES.RService,
+          TYPES.ALLOWED_COLLS,
+          TYPES.Progress
+        ],
+        true
+      )
+    )
   })
 }
--- workers/loc.api/di/types.js
+++ workers/loc.api/di/types.js
@@ -7,7 +7,14 @@ module.exports = {
   ALLOWED_COLLS: Symbol.for('ALLOWED_COLLS'),
   GRC_BFX_OPTS: Symbol.for('GRC_BFX_OPTS'),
   DataInserter: Symbol.for('DataInserter'),
+  DataInserterFactory: Symbol.for('DataInserterFactory'),
   WSTransport: Symbol.for('WSTransport'),
   WSEventEmitter: Symbol.for('WSEventEmitter'),
-  Sync: Symbol.for('Sync')
+  RedirectRequestsToApi: Symbol.for('RedirectRequestsToApi'),
+  SyncSchema: Symbol.for('SyncSchema'),
+  Sync: Symbol.for('Sync'),
+  SyncQueue: Symbol.for('SyncQueue'),
+  Progress: Symbol.for('Progress'),
+  DAO: Symbol.for('DAO'),
+  SqliteDAO: Symbol.for('SqliteDAO')
 }
