d6e6692bd73a07d620c8a3766376fd0ad353cd7c
Vladimir Voronkov <vsvoronkov@gmail.com>
2019-11-20 18:15:11 +0200

    Add migration v1

--- workers/loc.api/sync/dao/db-migrations/sqlite-migrations/migration.v1.js
+++ workers/loc.api/sync/dao/db-migrations/sqlite-migrations/migration.v1.js
@@ -1,22 +1,35 @@
 'use strinc'
 
 const AbstractMigration = require('./abstract.migration')
+const {
+  getTableCreationQuery
+} = require('../../helpers')
 
 class MigrationV1 extends AbstractMigration {
   /**
    * TODO:
    * @override
    */
-  up () {
+  up (modelsMap, TABLES_NAMES) {
     console.log('[migration.v1 is upped]'.bgBlue)
+
+    const dbConfigsModel = modelsMap.get(
+      TABLES_NAMES.DB_CONFIGS
+    )
+    const dbConfigsTableCreationQuery = getTableCreationQuery(
+      dbConfigsModel
+    )
+    this.addSql(dbConfigsTableCreationQuery)
   }
 
   /**
    * TODO:
    * @override
    */
-  down () {
+  down (modelsMap, TABLES_NAMES) {
     console.log('[migration.v1 is downed]'.bgBlue)
+
+    this.addSql(`DROP TABLE ${TABLES_NAMES.DB_CONFIGS}`)
   }
 }
 
