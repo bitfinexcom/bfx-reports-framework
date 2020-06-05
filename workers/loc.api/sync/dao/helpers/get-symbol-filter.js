66c05a3e4e1dc1995af0bacd9af5f43a51da01e1
Vladimir Voronkov <vsvoronkov@gmail.com>
2020-06-05 16:37:04 +0300

    Improve symbols filtering

--- workers/loc.api/sync/dao/helpers/get-symbol-filter.js
+++ workers/loc.api/sync/dao/helpers/get-symbol-filter.js
@@ -4,11 +4,12 @@ module.exports = (symbol, symbolFieldName) => {
   if (typeof symbol === 'string') {
     return { [symbolFieldName]: symbol }
   }
-  if (
-    Array.isArray(symbol) &&
-    symbol.length === 1
-  ) {
-    return { [symbolFieldName]: symbol[0] }
+  if (Array.isArray(symbol)) {
+    return {
+      [symbolFieldName]: symbol.length === 1
+        ? symbol[0]
+        : symbol
+    }
   }
 
   return {}
