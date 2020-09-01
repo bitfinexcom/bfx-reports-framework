e93739eb373f8524b58448002efb35314341a8fe
Vladimir Voronkov <vsvoronkov@gmail.com>
2020-09-01 14:43:39 +0300

    Improve requests redirection to api

--- workers/loc.api/sync/helpers/redirect-requests-to-api.js
+++ workers/loc.api/sync/helpers/redirect-requests-to-api.js
@@ -6,15 +6,29 @@ module.exports = (
   dao,
   TABLES_NAMES,
   wsEventEmitter
-) => async (state = true) => {
-  await dao.updateRecordOf(
-    TABLES_NAMES.SYNC_MODE,
-    { isEnable: !state }
-  )
+) => async ({
+  isRedirected = true,
+  isAuthSpecified = false,
+  auth
+}) => {
+  if (isAuthSpecified) {
+    await dao.updateRecordOf(
+      TABLES_NAMES.SYNC_MODE,
+      { isEnable: !isRedirected }
+    )
+  }
+
   await wsEventEmitter.emitRedirectingRequestsStatusToApi(
     (user) => {
+      if (
+        isAuthSpecified &&
+        wsEventEmitter.isInvalidAuth(auth, user)
+      ) {
+        return null
+      }
+
       return (
-        state ||
+        isRedirected ||
         isEmpty(user) ||
         !user.isDataFromDb
       )
