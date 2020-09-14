500984bb32acfbbe12ab3b5da9150e43e607ccee
Vladimir Voronkov <vsvoronkov@gmail.com>
2020-09-14 14:26:51 +0300

    Add v17 db migration to re-map 22 category fields

--- workers/loc.api/sync/schema/index.js
+++ workers/loc.api/sync/schema/index.js
@@ -7,7 +7,7 @@
  * in the `workers/loc.api/sync/dao/db-migrations/sqlite-migrations` folder,
  * e.g. `migration.v1.js`, where `v1` is `SUPPORTED_DB_VERSION`
  */
-const SUPPORTED_DB_VERSION = 16
+const SUPPORTED_DB_VERSION = 17
 
 const TABLES_NAMES = require('./tables-names')
 const ALLOWED_COLLS = require('./allowed.colls')
