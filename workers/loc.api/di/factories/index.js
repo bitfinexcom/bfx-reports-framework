fdaf76124d8aeeed331a1abd365249ce36bb3527
Vladimir Voronkov <vsvoronkov@gmail.com>
2021-01-25 20:38:55 +0200

    Add sync factory

--- workers/loc.api/di/factories/index.js
+++ workers/loc.api/di/factories/index.js
@@ -3,9 +3,11 @@
 const migrationsFactory = require('./migrations-factory')
 const dbMigratorFactory = require('./db-migrator-factory')
 const dataInserterFactory = require('./data-inserter-factory')
+const syncFactory = require('./sync-factory')
 
 module.exports = {
   migrationsFactory,
   dbMigratorFactory,
-  dataInserterFactory
+  dataInserterFactory,
+  syncFactory
 }
