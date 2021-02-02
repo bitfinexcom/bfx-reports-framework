8304e9f3340e353801efb7e6beedb32fbb9d12f4
Vladimir Voronkov <vsvoronkov@gmail.com>
2021-02-02 15:28:00 +0200

    Fix sync collections name getter

--- workers/loc.api/sync/data.inserter/helpers/get-sync-coll-name.js
+++ workers/loc.api/sync/data.inserter/helpers/get-sync-coll-name.js
@@ -1,11 +1,11 @@
 'use strict'
 
-const { upperCase, snakeCase } = require('lodash')
+const { snakeCase } = require('lodash')
 
 module.exports = (method) => {
   const name = method.replace(/^[^A-Z]+/, '')
-  const upperCaseName = upperCase(name)
-  const snakeCaseName = snakeCase(upperCaseName)
+  const snakeCaseName = snakeCase(name)
+  const upperCaseName = snakeCaseName.toUpperCase()
 
-  return snakeCaseName
+  return upperCaseName
 }
