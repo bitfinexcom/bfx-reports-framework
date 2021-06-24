2bc132535a4910a6aeb5fe9d6172dbe3b31799fc
Vladimir Voronkov <vsvoronkov@gmail.com>
2021-06-24 15:47:07 +0300

    Add week timeframe for getting start mts using moment

--- workers/loc.api/sync/helpers/get-start-mts-by-timeframe.js
+++ workers/loc.api/sync/helpers/get-start-mts-by-timeframe.js
@@ -1,24 +1,35 @@
 'use strict'
 
+const moment = require('moment')
+
 module.exports = (ts, timeframe = 'year') => {
-  const date = ts instanceof Date ? ts : new Date(ts)
-  const year = date.getUTCFullYear()
-  const month = date.getUTCMonth()
-  const day = date.getUTCDate()
+  const date = moment.utc(ts)
+
+  const year = date.year()
+  const month = date.month()
+  const weekYear = date.isoWeekYear()
+  const week = date.isoWeek()
+  const day = date.dayOfYear()
+
+  const momentDate = moment.utc({ year })
 
   if (timeframe === 'day') {
-    return Date.UTC(
-      year,
-      month,
-      day
-    )
+    momentDate.dayOfYear(day)
+
+    return momentDate.valueOf()
+  }
+  if (timeframe === 'week') {
+    momentDate.isoWeekYear(weekYear)
+    momentDate.isoWeekday(1)
+    momentDate.isoWeek(week)
+
+    return momentDate.valueOf()
   }
   if (timeframe === 'month') {
-    return Date.UTC(
-      year,
-      month
-    )
+    momentDate.month(month)
+
+    return momentDate.valueOf()
   }
 
-  return Date.UTC(year)
+  return momentDate.valueOf()
 }
