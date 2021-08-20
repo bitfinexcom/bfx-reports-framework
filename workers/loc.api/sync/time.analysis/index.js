93ae8516572616f3947d467a61cde1f48b1a665c
Vladimir Voronkov <vsvoronkov@gmail.com>
2021-08-20 16:28:05 +0300

    Check each table has-coll-been-synced-at-least-once for time analysis

--- workers/loc.api/sync/time.analysis/index.js
+++ workers/loc.api/sync/time.analysis/index.js
@@ -2,32 +2,82 @@
 
 const { decorateInjectable } = require('../../di/utils')
 
+const ANALYZED_TABLE_NAMES = require('./analyzed-table-names')
+
 const depsTypes = (TYPES) => [
   TYPES.DAO,
   TYPES.TABLES_NAMES,
   TYPES.SyncSchema,
-  TYPES.Authenticator
+  TYPES.Authenticator,
+  TYPES.SyncCollsManager
 ]
 class TimeAnalysis {
   constructor (
     dao,
     TABLES_NAMES,
     syncSchema,
-    authenticator
+    authenticator,
+    syncCollsManager
   ) {
     this.dao = dao
     this.TABLES_NAMES = TABLES_NAMES
     this.syncSchema = syncSchema
     this.authenticator = authenticator
+    this.syncCollsManager = syncCollsManager
 
     this._methodCollMap = this.syncSchema.getMethodCollMap()
   }
 
   async getTimeAnalysis (args = {}) {
-    const { auth = {} } = args
+    const auth = await this.authenticator
+      .verifyRequestUser(args)
 
-    const user = await this.authenticator
-      .verifyRequestUser({ auth })
+    const {
+      _id: userId,
+      subUsers,
+      isSubAccount
+    } = auth
+
+    const res = {}
+
+    for (const collName of ANALYZED_TABLE_NAMES) {
+      if (!isSubAccount) {
+        const isValid = await this.syncCollsManager
+          .hasCollBeenSyncedAtLeastOnce({
+            userId,
+            collName
+          })
+
+        if (!isValid) {
+          res[collName] = null
+
+          continue
+        }
+      }
+      if (isSubAccount) {
+        const areAllValidPromise = subUsers.map((subUser) => {
+          const { _id: subUserId } = { ...subUser }
+
+          return this.syncCollsManager
+            .hasCollBeenSyncedAtLeastOnce({
+              userId,
+              subUserId,
+              collName
+            })
+        })
+        const areAllValid = await Promise.all(areAllValidPromise)
+
+        if (areAllValid.some((isValid) => !isValid)) {
+          res[collName] = null
+
+          continue
+        }
+      }
+
+      // TODO: common check up
+    }
+
+    return res
   }
 }
 
