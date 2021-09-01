26217fc0f22a6cb7bb1ab71a1769b2905717f45c
Vladimir Voronkov <vsvoronkov@gmail.com>
2021-09-01 13:49:12 +0300

    Add invoice list table model

--- workers/loc.api/sync/schema/models.js
+++ workers/loc.api/sync/schema/models.js
@@ -611,6 +611,48 @@ const _models = new Map([
     }
   ],
   [
+    TABLES_NAMES.PAY_INVOICE_LIST,
+    {
+      _id: ID_PRIMARY_KEY,
+      id: 'VARCHAR(255)',
+      t: 'BIGINT',
+      duration: 'INT',
+      amount: 'DECIMAL(22,12)',
+      currency: 'VARCHAR(255)',
+      orderId: 'VARCHAR(255)',
+      payCurrencies: 'TEXT', // JSON
+      webhook: 'VARCHAR(255)',
+      redirectUrl: 'VARCHAR(255)',
+      status: 'VARCHAR(255)',
+      customerInfo: 'TEXT', // JSON
+      invoices: 'TEXT', // JSON
+      merchantName: 'VARCHAR(255)',
+
+      [UNIQUE_INDEX_FIELD_NAME]: ['id', 'user_id'],
+      [INDEX_FIELD_NAME]: [
+        ['user_id', 'currency', 't'],
+        ['user_id', 'id', 't'],
+        ['user_id', 't'],
+        ['user_id', 'subUserId', 't',
+          'WHERE subUserId IS NOT NULL'],
+        ['subUserId', 'id',
+          'WHERE subUserId IS NOT NULL']
+      ],
+      [CONSTR_FIELD_NAME]: [
+        `CONSTRAINT #{tableName}_fk_user_id
+        FOREIGN KEY (user_id)
+        REFERENCES ${TABLES_NAMES.USERS}(_id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE`,
+        `CONSTRAINT #{tableName}_fk_subUserId
+        FOREIGN KEY (subUserId)
+        REFERENCES ${TABLES_NAMES.USERS}(_id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE`
+      ]
+    }
+  ],
+  [
     TABLES_NAMES.TICKERS_HISTORY,
     {
       _id: ID_PRIMARY_KEY,
