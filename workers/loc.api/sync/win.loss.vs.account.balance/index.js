94cc106a0843823dd8752de51f68a3ccc97fe93d
Vladimir Voronkov <vsvoronkov@gmail.com>
2021-10-12 15:22:52 +0300

    Add ability to calc perc win-loss vs account balance

--- workers/loc.api/sync/win.loss.vs.account.balance/index.js
+++ workers/loc.api/sync/win.loss.vs.account.balance/index.js
@@ -58,7 +58,7 @@ class WinLossVSAccountBalance {
         plGroupedByTimeframe
       },
       false,
-      this._getWinLossByTimeframe(
+      this._winLossVSAccountBalanceByTimeframe(
         { isUnrealizedProfitExcluded }
       ),
       true
@@ -75,14 +75,67 @@ class WinLossVSAccountBalance {
     return res
   }
 
-  // TODO:
-  _getWinLossByTimeframe ({ isUnrealizedProfitExcluded }) {
+  _winLossVSAccountBalanceByTimeframe ({
+    isUnrealizedProfitExcluded
+  }) {
+    let firstWalletsVals = {}
+    let firstPLVals = 0
+    let prevMovementsRes = 0
+
     return ({
       walletsGroupedByTimeframe = {},
       withdrawalsGroupedByTimeframe = {},
       depositsGroupedByTimeframe = {},
       plGroupedByTimeframe = {}
-    } = {}, i, arr) => {}
+    } = {}, i, arr) => {
+      const symb = 'USD'
+      const isFirst = (i + 1) === arr.length
+
+      if (isFirst) {
+        firstWalletsVals = walletsGroupedByTimeframe
+        firstPLVals = plGroupedByTimeframe
+      }
+
+      prevMovementsRes = this.winLoss.sumMovementsWithPrevRes(
+        prevMovementsRes,
+        withdrawalsGroupedByTimeframe,
+        depositsGroupedByTimeframe
+      )
+
+      const movements = Number.isFinite(prevMovementsRes[symb])
+        ? prevMovementsRes[symb]
+        : 0
+      const firstWallets = Number.isFinite(firstWalletsVals[symb])
+        ? firstWalletsVals[symb]
+        : 0
+      const wallets = Number.isFinite(walletsGroupedByTimeframe[symb])
+        ? walletsGroupedByTimeframe[symb]
+        : 0
+      const firstPL = Number.isFinite(firstPLVals[symb])
+        ? firstPLVals[symb]
+        : 0
+      const pl = Number.isFinite(plGroupedByTimeframe[symb])
+        ? plGroupedByTimeframe[symb]
+        : 0
+
+      const realized = (wallets - movements) - firstWallets
+      const unrealized = isUnrealizedProfitExcluded
+        ? 0
+        : pl - firstPL
+
+      const winLoss = realized + unrealized
+
+      if (
+        !Number.isFinite(winLoss) ||
+        firstWallets === 0
+      ) {
+        return { perc: 0 }
+      }
+
+      const perc = winLoss / firstWallets
+
+      return { perc }
+    }
   }
 }
 
