afdba349852a9627240adbb7a1a68edba38239b5
Vladimir Voronkov <vsvoronkov@gmail.com>
2021-10-13 10:34:33 +0300

    Add test coverage for win-loss vs account balance csv report

--- test/test-cases/additional-api-sync-mode-sqlite-test-cases.js
+++ test/test-cases/additional-api-sync-mode-sqlite-test-cases.js
@@ -907,4 +907,30 @@ module.exports = (
 
     await testMethodOfGettingCsv(procPromise, aggrPromise, res)
   })
+
+  it('it should be successfully performed by the getWinLossVSAccountBalanceCsv method', async function () {
+    this.timeout(60000)
+
+    const procPromise = queueToPromise(params.processorQueue)
+    const aggrPromise = queueToPromise(params.aggregatorQueue)
+
+    const res = await agent
+      .post(`${basePath}/json-rpc`)
+      .type('json')
+      .send({
+        auth,
+        method: 'getWinLossVSAccountBalanceCsv',
+        params: {
+          end,
+          start,
+          timeframe: 'day',
+          email
+        },
+        id: 5
+      })
+      .expect('Content-Type', /json/)
+      .expect(200)
+
+    await testMethodOfGettingCsv(procPromise, aggrPromise, res)
+  })
 }
