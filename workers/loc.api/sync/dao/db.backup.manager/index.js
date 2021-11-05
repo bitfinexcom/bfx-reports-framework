59c24501853fd0a459a6e1a6f5f859652f6a599b
Vladimir Voronkov <vsvoronkov@gmail.com>
2021-11-05 13:07:39 +0200

    Add ability to get backup files metadata in folder

--- workers/loc.api/sync/dao/db.backup.manager/index.js
+++ workers/loc.api/sync/dao/db.backup.manager/index.js
@@ -1,7 +1,9 @@
 'use strict'
 
 const { mkdirSync } = require('fs')
+const { readdir } = require('fs/promises')
 const path = require('path')
+const moment = require('moment')
 
 const { decorateInjectable } = require('../../../di/utils')
 
@@ -45,7 +47,59 @@ class DBBackupManager {
       ? new Date(mts).toISOString()
       : new Date().toISOString()
 
-    return `backup-v${version}-${isoTS}.db`
+    return `backup_v${version}_${isoTS}.db`
+  }
+
+  async _getBackupFilesMetadata () {
+    const files = await readdir(
+      this._backupFolder,
+      { withFileTypes: true }
+    )
+    const filteredFiles = files.reduce((accum, dirent) => {
+      const { name } = dirent
+
+      if (
+        dirent.isFile() &&
+        name.startsWith('backup') &&
+        name.endsWith('.db')
+      ) {
+        let version = null
+        let mts = 0
+
+        const trimmedName = name.replace(/\.db$/i, '')
+        const chancks = trimmedName.split('_')
+
+        for (const chanck of chancks) {
+          const momentDate = moment(chanck, moment.ISO_8601, true)
+
+          if (/^v\d+$/i.test(chanck)) {
+            const trimmedChanck = chanck.replace(/^v/i, '')
+            const number = Number.parseInt(trimmedChanck)
+
+            version = Number.isInteger(number)
+              ? number
+              : null
+          }
+          if (momentDate.isValid()) {
+            mts = momentDate.valueOf()
+          }
+        }
+
+        if (!Number.isInteger(version)) {
+          return accum
+        }
+
+        accum.push({
+          name,
+          version,
+          mts
+        })
+      }
+
+      return accum
+    }, [])
+
+    return filteredFiles
   }
 }
 
