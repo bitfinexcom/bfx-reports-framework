1b2a433906848663bf6dc4e0c41b3aae297e4065
Vladimir Voronkov <vsvoronkov@gmail.com>
2021-11-12 10:25:52 +0200

    Add error:migration process message

--- workers/loc.api/process.message.manager/process.messages.js
+++ workers/loc.api/process.message.manager/process.messages.js
@@ -2,5 +2,6 @@
 
 module.exports = {
   READY_WORKER: 'ready:worker',
-  ALL_TABLE_HAVE_NOT_BEEN_CLEARED: 'all-tables-have-not-been-cleared'
+  ALL_TABLE_HAVE_NOT_BEEN_CLEARED: 'all-tables-have-not-been-cleared',
+  ERROR_MIGRATIONS: 'error:migrations'
 }
