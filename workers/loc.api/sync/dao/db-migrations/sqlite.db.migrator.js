c04c97579c58cbf6717e3ae48d85c718d1ac09b4
Vladimir Voronkov <vsvoronkov@gmail.com>
2021-11-15 16:15:09 +0200

    Improve migration flow when fail

--- workers/loc.api/sync/dao/db-migrations/sqlite.db.migrator.js
+++ workers/loc.api/sync/dao/db-migrations/sqlite.db.migrator.js
@@ -27,48 +27,44 @@ class SqliteDbMigrator extends DbMigrator {
         throw err
       }
 
-      const isDone = await this.dbBackupManager.restoreDb()
+      const responsePromise = this.processMessageManager.addStateToWait(
+        this.processMessageManager.PROCESS_STATES.RESPONSE_MIGRATION_HAS_FAILED_WHAT_SHOULD_BE_DONE
+      )
+      this.processMessageManager.sendState(
+        this.processMessageManager.PROCESS_MESSAGES.REQUEST_MIGRATION_HAS_FAILED_WHAT_SHOULD_BE_DONE
+      )
+      const {
+        shouldRestore = false,
+        shouldRemove = false
+      } = (await responsePromise) ?? {}
 
-      if (isDone) {
+      if (shouldRestore) {
+        const isDbRestored = await this.dbBackupManager.restoreDb()
+
+        if (isDbRestored) {
+          return
+        }
+
+        const rmDbPromise = this.processMessageManager.addStateToWait(
+          this.processMessageManager.PROCESS_STATES.REMOVE_ALL_TABLES
+        )
+        this.processMessageManager.sendState(
+          this.processMessageManager.PROCESS_MESSAGES.REQUEST_SHOULD_ALL_TABLES_BE_REMOVED
+        )
+
+        await rmDbPromise
+
+        return
+      }
+      if (shouldRemove) {
+        await this.processMessageManager.processState(
+          this.processMessageManager.PROCESS_STATES.REMOVE_ALL_TABLES
+        )
+
         return
       }
 
-      // TODO: Need to collect whole process messaging to single module
-      await new Promise((resolve, reject) => {
-        const handler = async (mess) => {
-          try {
-            const { state } = { ...mess }
-
-            if (state === 'migration:dont-remove-all-tables') {
-              resolve()
-
-              return
-            }
-            if (state !== 'migration:remove-all-tables') {
-              return
-            }
-
-            await this.removeAllTables()
-            this.logger.debug('[All tables have been removed]')
-
-            process.send({ state: 'migration:all-tables-have-been-removed' })
-            resolve()
-          } catch (err) {
-            this.logger.error(err)
-
-            process.send({ state: 'migration:all-tables-have-not-been-removed' })
-            reject(err)
-          }
-        }
-
-        process.on('message', handler)
-        setTimeout(() => {
-          process.off('message', handler)
-          process.send({ state: 'migration:all-tables-have-not-been-removed' })
-          resolve()
-        }, 30000).unref()
-        process.send({ state: 'migration:should-all-tables-be-removed' })
-      })
+      throw err
     }
   }
 
