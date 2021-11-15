0aab665a69ad0af1034a7570231541866735d7cd
Vladimir Voronkov <vsvoronkov@gmail.com>
2021-11-15 16:14:05 +0200

    Add process states

--- workers/loc.api/process.message.manager/process.states.js
+++ workers/loc.api/process.message.manager/process.states.js
@@ -1,5 +1,8 @@
 'use strict'
 
 module.exports = {
-  CLEAR_ALL_TABLES: 'clear-all-tables'
+  CLEAR_ALL_TABLES: 'clear-all-tables',
+  REMOVE_ALL_TABLES: 'remove-all-tables',
+  RESPONSE_MIGRATION_HAS_FAILED_WHAT_SHOULD_BE_DONE: 'response:migration-has-failed:what-should-be-done'
+
 }
