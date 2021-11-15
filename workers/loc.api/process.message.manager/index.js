59c2c29f493a9500ce7e61090eb41f1553a31079
Vladimir Voronkov <vsvoronkov@gmail.com>
2021-11-15 16:14:42 +0200

    Add ability to remove all tables via ipc

--- workers/loc.api/process.message.manager/index.js
+++ workers/loc.api/process.message.manager/index.js
@@ -41,18 +41,24 @@ class ProcessMessageManager {
     this.PROCESS_MESSAGES = PROCESS_MESSAGES
     this.SET_PROCESS_STATES = new Set(Object.values(PROCESS_STATES))
     this.SET_PROCESS_MESSAGES = new Set(Object.values(PROCESS_MESSAGES))
+
+    this._promisesToWait = new Map(
+      [...this.SET_PROCESS_STATES].map((state) => ([state, []]))
+    )
+
+    this._mainHandler = null
   }
 
   init () {
-    onMessage(async (err, state, data) => {
-      if (
-        !this.SET_PROCESS_STATES.has(state) ||
-        typeof this[state] !== 'function'
-      ) {
+    this._mainHandler = onMessage(async (err, state, data) => {
+      if (!this.SET_PROCESS_STATES.has(state)) {
         return
       }
+      if (typeof this[state] === 'function') {
+        await this[state](err, state, data)
+      }
 
-      await this[state](err, state, data)
+      this._processPromise(err, state, data)
     }, this.logger)
   }
 
@@ -70,6 +76,56 @@ class ProcessMessageManager {
     return onMessage(...args, this.logger)
   }
 
+  addStateToWait (state) {
+    if (!this.SET_PROCESS_STATES.has(state)) {
+      throw new ProcessStateSendingError()
+    }
+
+    const promise = new Promise((resolve, reject) => {
+      const queue = this._promisesToWait.get(state)
+
+      queue.push({
+        promise,
+        resolve,
+        reject
+      })
+    })
+
+    return promise
+  }
+
+  async processState (state, data) {
+    await this._mainHandler({ state, data })
+  }
+
+  _processPromise (err, state, data) {
+    const queue = this._promisesToWait.get(state)
+
+    if (
+      !Array.isArray(queue) ||
+      queue.length === 0
+    ) {
+      return
+    }
+
+    for (const [i, task] of queue.entries()) {
+      const {
+        resolve,
+        reject
+      } = task
+
+      queue.splice(i, 1)
+
+      if (err) {
+        reject(err)
+
+        continue
+      }
+
+      resolve(data)
+    }
+  }
+
   async [PROCESS_STATES.CLEAR_ALL_TABLES] (err, state, data) {
     if (err) {
       this.sendState(PROCESS_MESSAGES.ALL_TABLE_HAVE_NOT_BEEN_CLEARED)
@@ -85,6 +141,30 @@ class ProcessMessageManager {
     })
 
     this.sendState(PROCESS_MESSAGES.ALL_TABLE_HAVE_BEEN_CLEARED)
+  }
+
+  async [PROCESS_STATES.REMOVE_ALL_TABLES] (err, state, data) {
+    if (err) {
+      this.sendState(PROCESS_MESSAGES.ALL_TABLE_HAVE_NOT_BEEN_REMOVED)
+
+      return
+    }
+
+    await this.dao.disableForeignKeys()
+
+    try {
+      await this.dao.dropAllTables()
+      await this.dao.setCurrDbVer(0)
+    } catch (err) {
+      await this.dao.enableForeignKeys()
+
+      throw err
+    }
+
+    await this.dao.enableForeignKeys()
+
+    this.logger.debug('[All tables have been removed]')
+    this.sendState(PROCESS_MESSAGES.ALL_TABLE_HAVE_BEEN_REMOVED)
   }
 }
 
