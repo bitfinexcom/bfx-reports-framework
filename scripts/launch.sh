eebd5658d6c253322dc1b2394b070f99ec8be95a
Vladimir Voronkov <vsvoronkov@gmail.com>
2022-01-19 18:55:30 +0200

    Add cli bash script to launch/relaunch docker-compose services

---
+++ scripts/launch.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+COLOR_RED="\033[31m"
+COLOR_GREEN="\033[32m"
+COLOR_BLUE="\033[34m"
+COLOR_NORMAL="\033[39m"
+
+programname=$0
+
+launchAll=0
+launchGrapes=0
+launchWorker=0
+launchExpress=0
+launchNginx=0
+buildUI=0
+detachedMode=""
+
+function usage {
+  echo -e "\
+\n${COLOR_GREEN}Usage: $programname [options] [-d] | [-h]${COLOR_BLUE}
+\nOptions:
+  -a    Launch all repositories
+  -g    Launch grenache-grape network only
+  -w    Launch bfx-reports-framework worker only
+  -e    Launch bfx-report-express server only
+  -n    Launch NGINX reverse proxy server only
+  -u    Build bfx-report-ui static files only
+  -d    Detached mode: Run containers in the background
+  -h    Display help\
+${COLOR_NORMAL}" 1>&2
+}
+
+if [ $# == 0 ]; then
+  usage
+  exit 1
+fi
+
+while getopts "agwenudh" opt; do
+  case "${opt}" in
+    a) launchAll=1;;
+    g) launchGrapes=1;;
+    w) launchWorker=1;;
+    e) launchExpress=1;;
+    n) launchNginx=1;;
+    u) buildUI=1;;
+    d) detachedMode="-d";;
+    h)
+      usage
+      exit 0
+      ;;
+    *)
+      echo -e "\n${COLOR_RED}No reasonable options found!${COLOR_NORMAL}"
+      usage
+      exit 1
+      ;;
+  esac
+done
+
+if [ $launchGrapes == 1 ] \
+  && [ $launchWorker == 1 ] \
+  && [ $launchExpress == 1 ] \
+  && [ $launchNginx == 1 ] \
+  && [ $buildUI == 1 ]
+then
+  launchAll=1
+  launchGrapes=0
+  launchWorker=0
+  launchExpress=0
+  launchNginx=0
+  buildUI=0
+fi
+
+composeCommonFlags="\
+  --build \
+  --force-recreate \
+  --remove-orphans \
+  --timeout 2 \
+  $detachedMode \
+"
+
+if [ $launchAll == 1 ]; then
+  docker-compose up $composeCommonFlags \
+
+  exit 0
+fi
+
+composeCommonFlags="\
+  --no-deps \
+  $composeCommonFlags \
+"
+
+if [ $launchGrapes == 1 ]; then
+  docker-compose up $composeCommonFlags \
+    grape1 grape2
+fi
+if [ $launchWorker == 1 ]; then
+  runningServices=$(docker-compose ps --filter "status=running" --services)
+  isGrape1Running=$(echo "$runningServices" | { grep 'grape1' || test $? = 1; } | wc -l)
+  isGrape2Running=$(echo "$runningServices" | { grep 'grape2' || test $? = 1; } | wc -l)
+  grapesServices=""
+
+  if [ $isGrape1Running == 0 ] && [ $isGrape1Running == 0 ]; then
+    grapesServices="grape1 grape2"
+  fi
+
+  docker-compose up $composeCommonFlags \
+    $grapesServices worker
+fi
+if [ $launchExpress == 1 ]; then
+  docker-compose up $composeCommonFlags \
+    express
+fi
+if [ $launchNginx == 1 ]; then
+  docker-compose up $composeCommonFlags \
+    nginx
+fi
+if [ $buildUI == 1 ]; then
+  docker-compose up $composeCommonFlags \
+    ui-builder
+fi
