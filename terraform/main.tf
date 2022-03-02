33b7982727b75fbfe57fcdf11c27a1f84931dfc4
Vladimir Voronkov <vsvoronkov@gmail.com>
2022-03-02 14:30:24 +0200

    Use network module

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,6 +12,14 @@ locals {
   )
 }
 
+module "network" {
+  source = "./modules/network"
+  namespace = var.namespace
+  vpc_cidr = var.aws_vpc_cidr
+  common_tags = var.common_tags
+  allowed_ports = var.allowed_ports
+}
+
 resource "aws_instance" "bfx_reports_framework_ubuntu" {
   ami = data.aws_ami.ubuntu.id
   instance_type = var.aws_instance_type
@@ -23,12 +31,15 @@ resource "aws_instance" "bfx_reports_framework_ubuntu" {
     repo_fork = var.repo_fork
     repo_branch = var.repo_branch
     nginx_port = var.nginx_port
-    nginx_host = aws_eip.bfx_reports_framework_eip.public_dns
+    nginx_host = module.network.public_dns
     secret_key = var.secret_key # TODO: AWS SSM Parameter Store
   })
 
-  vpc_security_group_ids = [aws_security_group.bfx_sec_gr.id]
-  subnet_id = aws_subnet.bfx_subnet.id
+  vpc_security_group_ids = [module.network.sec_gr_pub_id]
+  subnet_id = module.network.vpc.public_subnets[0]
+  associate_public_ip_address = true
+
+  key_name = aws_key_pair.bfx_ssh_key.key_name
 
   lifecycle {
     create_before_destroy = true
@@ -37,83 +48,6 @@ resource "aws_instance" "bfx_reports_framework_ubuntu" {
   tags = merge(
     local.common_tags,
     { Name = "BFXReportsFrameworkInstance" }
-  )
-}
-
-resource "aws_eip_association" "bfx_reports_framework_eip_assoc" {
-  instance_id   = aws_instance.bfx_reports_framework_ubuntu.id
-  allocation_id = aws_eip.bfx_reports_framework_eip.id
-}
-
-resource "aws_eip" "bfx_reports_framework_eip" {
-  vpc = true
-
-  depends_on = [aws_internet_gateway.bfx_gw]
-
-  tags = merge(
-    local.common_tags,
-    { Name = "BFXReportsFrameworkEIP" }
-  )
-}
-
-resource "aws_security_group" "bfx_sec_gr" {
-  name = "BFXSecurityGroup"
-  description = "Allow traffic"
-  vpc_id = aws_vpc.bfx_vpc.id
-
-  dynamic "ingress" {
-    for_each = var.allowed_ports
-    content {
-      from_port = ingress.value
-      to_port = ingress.value
-      protocol = "tcp"
-      cidr_blocks = [aws_vpc.bfx_vpc.cidr_block]
-    }
-  }
-
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = merge(
-    local.common_tags,
-    { Name = "BFXSecurityGroup" }
-  )
-}
-
-resource "aws_vpc" "bfx_vpc" {
-  cidr_block = var.aws_vpc_cidr
-  enable_dns_hostnames = true
-  enable_dns_support = true
-
-  tags = merge(
-    local.common_tags,
-    { Name = "BFX_VPC" }
-  )
-}
-
-resource "aws_internet_gateway" "bfx_gw" {
-  vpc_id = aws_vpc.bfx_vpc.id
-
-  tags = merge(
-    local.common_tags,
-    { Name = "BFXGateway" }
-  )
-}
-
-resource "aws_subnet" "bfx_subnet" {
-  vpc_id = aws_vpc.bfx_vpc.id
-  cidr_block = cidrsubnet(aws_vpc.bfx_vpc.cidr_block, 8, 10)
-  map_public_ip_on_launch = true
-
-  depends_on = [aws_internet_gateway.bfx_gw]
-
-  tags = merge(
-    local.common_tags,
-    { Name = "BFXSubnet" }
   )
 }
 
