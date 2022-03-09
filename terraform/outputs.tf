45bb3baa4493ea9ea08d038ea456e48acdf244be
Vladimir Voronkov <vsvoronkov@gmail.com>
2022-03-09 12:30:52 +0200

    Fix main terraform output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,7 @@
 output bfx_reports_framework_pub_ip {
-  value = aws_instance.bfx_reports_framework_ubuntu.public_ip
+  value = module.ec2.instance_pub_ip
 }
 
 output bfx_reports_framework_pub_dns {
-  value = aws_instance.bfx_reports_framework_ubuntu.public_dns
+  value = module.ec2.instance_pub_dns
 }
