dc8db939d941053483fa3ccb3fce0e159a7770de
Vladimir Voronkov <vsvoronkov@gmail.com>
2022-03-22 13:55:41 +0200

    Add aws kms key variables

---
+++ terraform/modules/kms_key/variables.tf
@@ -0,0 +1,34 @@
+variable "namespace" {
+  type = string
+  default = "Custom"
+}
+
+variable "customer_master_key_spec" {
+  type = string
+  description = "Key specs for KMS keys"
+  default = "SYMMETRIC_DEFAULT"
+}
+
+variable "enable_key_rotation" {
+  type = bool
+  description = "Specifies whether key rotation is enabled"
+  default = true
+}
+
+variable "is_enabled" {
+  type = bool
+  description = "Specifies whether the key is enabled"
+  default = true
+}
+
+variable "multi_region" {
+  type = bool
+  description = "Indicates whether the KMS key is a multi-Region"
+  default = false
+}
+
+variable "common_tags" {
+  type = map
+  description = "Common tags"
+  default = {}
+}
