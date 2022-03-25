da77e91610ac5d34c732d92f80123e01fbb2b16a
Vladimir Voronkov <vsvoronkov@gmail.com>
2022-03-25 15:31:39 +0200

    Update terraform/aws versions

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.1.5"
+  required_version = ">= 1.1.7"
 
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "~> 4.1.0"
+      version = "~> 4.7.0"
     }
   }
 }
