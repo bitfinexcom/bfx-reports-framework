83055476f89a2a4e58a052224b116bab08d75cc6
Vladimir Voronkov <vsvoronkov@gmail.com>
2022-03-29 15:48:04 +0300

    Add credentials file to aws provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  shared_credentials_files = ["config/credentials.conf"]
+  profile = "default"
   region = var.aws_region
 }
 
 provider "aws" {
   alias  = "replica"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  shared_credentials_files = ["config/credentials.conf"]
+  profile = "default"
   region = var.aws_replica_region
 }
