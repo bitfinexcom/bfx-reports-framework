33d0c2a491ddf9fbf15814ad6230f68e4bc614b8
Vladimir Voronkov <vsvoronkov@gmail.com>
2022-08-19 11:00:16 +0300

    Create SyncTempTablesManager service

---
+++ workers/loc.api/sync/data.inserter/sync.temp.tables.manager/index.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const { decorateInjectable } = require('../../di/utils')
+
+const depsTypes = (TYPES) => [
+  TYPES.DAO,
+  TYPES.TABLES_NAMES,
+  TYPES.SyncSchema,
+  TYPES.SYNC_API_METHODS
+]
+class SyncTempTablesManager {
+  constructor (
+    dao,
+    TABLES_NAMES,
+    syncSchema,
+    SYNC_API_METHODS
+  ) {
+    this.dao = dao
+    this.TABLES_NAMES = TABLES_NAMES
+    this.syncSchema = syncSchema
+    this.SYNC_API_METHODS = SYNC_API_METHODS
+
+    this._methodCollMap = this.syncSchema.getMethodCollMap()
+  }
+}
+
+decorateInjectable(SyncTempTablesManager, depsTypes)
+
+module.exports = SyncTempTablesManager
