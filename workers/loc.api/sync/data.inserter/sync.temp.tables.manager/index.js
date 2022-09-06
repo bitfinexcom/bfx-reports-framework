fc5418505296b11794efc6ab498f3e535d5f0f8d
Vladimir Voronkov <vsvoronkov@gmail.com>
2022-09-06 13:59:14 +0300

    Add ability to clean up temp db structure

--- workers/loc.api/sync/data.inserter/sync.temp.tables.manager/index.js
+++ workers/loc.api/sync/data.inserter/sync.temp.tables.manager/index.js
@@ -8,19 +8,22 @@ const depsTypes = (TYPES) => [
   TYPES.DAO,
   TYPES.TABLES_NAMES,
   TYPES.SyncSchema,
-  TYPES.SYNC_API_METHODS
+  TYPES.SYNC_API_METHODS,
+  TYPES.SYNC_QUEUE_STATES
 ]
 class SyncTempTablesManager {
   constructor (
     dao,
     TABLES_NAMES,
     syncSchema,
-    SYNC_API_METHODS
+    SYNC_API_METHODS,
+    SYNC_QUEUE_STATES
   ) {
     this.dao = dao
     this.TABLES_NAMES = TABLES_NAMES
     this.syncSchema = syncSchema
     this.SYNC_API_METHODS = SYNC_API_METHODS
+    this.SYNC_QUEUE_STATES = SYNC_QUEUE_STATES
 
     this.syncQueueId = null
 
@@ -43,8 +46,32 @@ class SyncTempTablesManager {
     await this.dao.createDBStructure({ models, namePrefix })
   }
 
-  _getNamePrefix () {
-    return `temp_s${this.syncQueueId}_`
+  async cleanUpTempDBStructure () {
+    /*
+     * Don't remove temp DB tables of sync queue which can be processed
+     * or/and sync can be continued in the future
+     */
+    const activeSyncs = await this.dao.getElemsInCollBy(
+      this.TABLES_NAMES.SYNC_QUEUE,
+      {
+        sort: [['_id', 1]],
+        filter: {
+          state: [
+            this.SYNC_QUEUE_STATES.NEW_JOB_STATE,
+            this.SYNC_QUEUE_STATES.LOCKED_JOB_STATE,
+            this.SYNC_QUEUE_STATES.ERROR_JOB_STATE
+          ]
+        }
+      }
+    )
+    const exceptions = activeSyncs
+      .map(({ _id }) => this._getNamePrefix(_id))
+
+    await this.dao.dropAllTables({ exceptions })
+  }
+
+  _getNamePrefix (id) {
+    return `temp_s${id ?? this.syncQueueId}_`
   }
 }
 
