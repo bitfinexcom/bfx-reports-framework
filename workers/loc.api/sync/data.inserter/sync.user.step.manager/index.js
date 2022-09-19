4de1c4899a60415f18496788624dc0b17980fcf0
Vladimir Voronkov <vsvoronkov@gmail.com>
2022-09-19 15:16:25 +0300

    Add ability to get last synced info for curr coll

--- workers/loc.api/sync/data.inserter/sync.user.step.manager/index.js
+++ workers/loc.api/sync/data.inserter/sync.user.step.manager/index.js
@@ -1,5 +1,10 @@
 'use strict'
 
+const {
+  isInsertableArrObjTypeOfColl
+} = require('../../schema/utils')
+const SyncTempTablesManager = require('../sync.temp.tables.manager')
+
 const { decorateInjectable } = require('../../../di/utils')
 
 const { SyncQueueIDSettingError } = require('../../../errors')
@@ -33,6 +38,68 @@ class SyncUserStepManager {
     }
 
     this.syncQueueId = params.syncQueueId
+  }
+
+  // TODO:
+  async getLastSyncedInfoForCurrColl (syncSchema, params) {
+    if (!isInsertableArrObjTypeOfColl(syncSchema)) {
+      throw new Error('ERR_') // TODO:
+    }
+
+    const {
+      collName,
+      userId,
+      subUserId
+    } = params ?? {}
+
+    const hasSubUserIdField = (
+      typeof syncSchema?.model?.subUserId === 'string' &&
+      Number.isInteger(subUserId)
+    )
+    const tableName = syncSchema.name
+    const tempTableName = this._getCurrNamePrefix()
+    const hasTempTable = await this.dao.hasTable(tempTableName)
+
+    const userIdFilter = hasSubUserIdField
+      ? { $eq: { user_id: userId, subUserId } }
+      : { $eq: { user_id: userId } }
+
+    const syncUserStepInfoPromise = this.dao.getElemInCollBy(
+      this.TABLES_NAMES.SYNC_USER_STEPS,
+      {
+        collName,
+        ...userIdFilter
+      },
+      [['syncedAt', -1]]
+    )
+    const lastElemFromMainTablePromise = this.dao.getElemInCollBy(
+      tableName,
+      userIdFilter,
+      syncSchema.sort
+    )
+    const lastElemFromTempTablePromise = hasTempTable
+      ? this.dao.getElemInCollBy(
+          tempTableName,
+          userIdFilter,
+          syncSchema.sort
+        )
+      : null
+
+    const [
+      syncUserStepInfo,
+      lastElemFromMainTable,
+      lastElemFromTempTable
+    ] = Promise.all([
+      syncUserStepInfoPromise,
+      lastElemFromMainTablePromise,
+      lastElemFromTempTablePromise
+    ])
+  }
+
+  _getCurrNamePrefix (id) {
+    const syncQueueId = id ?? this.syncQueueId
+
+    return SyncTempTablesManager._getNamePrefix(syncQueueId)
   }
 }
 
