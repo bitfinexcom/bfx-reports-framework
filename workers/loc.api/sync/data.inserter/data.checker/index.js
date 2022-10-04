e67fc7c96794bdd8fd7b50ce0ced8e40cdf07be7
Vladimir Voronkov <vsvoronkov@gmail.com>
2022-10-04 11:49:52 +0300

    Add ability to detect that start sync mts was changed for candles

--- workers/loc.api/sync/data.inserter/data.checker/index.js
+++ workers/loc.api/sync/data.inserter/data.checker/index.js
@@ -697,20 +697,42 @@ class DataChecker {
         schema.start.push(syncUserStepData)
       }
 
+      const wasStartPointChanged = this._wasStartPointChanged(
+        syncUserStepData,
+        firstElemLedgers.mts
+      )
       const shouldFreshSyncBeAdded = this._shouldFreshSyncBeAdded(
         syncUserStepData,
         currMts
       )
 
-      if (!shouldFreshSyncBeAdded) {
+      if (
+        !wasStartPointChanged &&
+        !shouldFreshSyncBeAdded
+      ) {
         return
       }
 
       const freshSyncUserStepData = this.syncUserStepDataFactory({
-        currStart: lastElemMtsFromTables,
-        currEnd: currMts,
-        isCurrStepReady: false
+        isBaseStepReady: true,
+        isCurrStepReady: true
       })
+
+      if (wasStartPointChanged) {
+        freshSyncUserStepData.setParams({
+          baseStart: firstElemLedgers.mts,
+          baseEnd: syncUserStepData.baseStart,
+          isBaseStepReady: false
+        })
+      }
+      if (shouldFreshSyncBeAdded) {
+        freshSyncUserStepData.setParams({
+          currStart: lastElemMtsFromTables,
+          currEnd: currMts,
+          isCurrStepReady: false
+        })
+      }
+
       schema.hasNewData = true
       schema.start.push(freshSyncUserStepData)
     }
