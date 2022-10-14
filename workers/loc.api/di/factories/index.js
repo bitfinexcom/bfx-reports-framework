8ae463b6f3698ccbb73b389ca42032eb54270196
Vladimir Voronkov <vsvoronkov@gmail.com>
2022-10-14 11:05:57 +0300

    Fix exporting sync user step data factory

--- workers/loc.api/di/factories/index.js
+++ workers/loc.api/di/factories/index.js
@@ -5,11 +5,13 @@ const dbMigratorFactory = require('./db-migrator-factory')
 const dataInserterFactory = require('./data-inserter-factory')
 const syncFactory = require('./sync-factory')
 const processMessageManagerFactory = require('./process-message-manager-factory')
+const syncUserStepDataFactory = require('./sync-user-step-data-factory')
 
 module.exports = {
   migrationsFactory,
   dbMigratorFactory,
   dataInserterFactory,
   syncFactory,
-  processMessageManagerFactory
+  processMessageManagerFactory,
+  syncUserStepDataFactory
 }
