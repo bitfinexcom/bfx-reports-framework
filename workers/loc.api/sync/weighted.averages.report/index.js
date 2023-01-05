d337366a49a958e69647ae4a33a355cfabfc2366
Vladimir Voronkov <vsvoronkov@gmail.com>
2023-01-05 14:05:06 +0200

    Implement trades weighted price calc

--- workers/loc.api/sync/weighted.averages.report/index.js
+++ workers/loc.api/sync/weighted.averages.report/index.js
@@ -51,28 +51,9 @@ class WeightedAveragesReport {
       end,
       symbol
     })
+    const calcedTrades = this._calcTrades(trades)
 
-    // TODO: example returned data structure
-    return [
-      {
-        symbol: 'tBTCUSD',
-        buyingWeightedPrice: 123.321,
-        buyingAmount: 0.321,
-        sellingWeightedPrice: 123.321,
-        sellingAmount: 0.321,
-        cumulativeWeightedPrice: 123.321,
-        cumulativeAmount: 0.321
-      },
-      {
-        symbol: 'tETHUSD',
-        buyingWeightedPrice: 321.123,
-        buyingAmount: 0.123,
-        sellingWeightedPrice: 321.123,
-        sellingAmount: 0.123,
-        cumulativeWeightedPrice: 321.123,
-        cumulativeAmount: 0.123
-      }
-    ]
+    return calcedTrades
   }
 
   async _getTrades (args) {
@@ -105,6 +86,108 @@ class WeightedAveragesReport {
         isExcludePrivate: true
       }
     )
+  }
+
+  _calcTrades (trades = []) {
+    const symbResMap = new Map()
+
+    for (const trade of trades) {
+      const {
+        symbol,
+        execAmount,
+        execPrice
+      } = trade ?? {}
+
+      if (
+        !symbol ||
+        typeof symbol !== 'string' ||
+        !Number.isFinite(execAmount) ||
+        execAmount === 0 ||
+        !Number.isFinite(execPrice) ||
+        execPrice === 0
+      ) {
+        continue
+      }
+
+      const isBuying = execAmount > 0
+      const spent = execAmount * execPrice
+
+      const existedSymbRes = symbResMap.get(symbol)
+      const {
+        sumSpent: _sumSpent = 0,
+        sumAmount: _sumAmount = 0,
+        sumBuyingSpent: _sumBuyingSpent = 0,
+        sumBuyingAmount: _sumBuyingAmount = 0,
+        sumSellingSpent: _sumSellingSpent = 0,
+        sumSellingAmount: _sumSellingAmount = 0
+      } = existedSymbRes ?? {}
+
+      const sumSpent = Number.isFinite(spent)
+        ? _sumSpent + spent
+        : _sumSpent
+      const sumAmount = _sumAmount + execAmount
+      const sumBuyingSpent = (
+        isBuying &&
+        Number.isFinite(spent)
+      )
+        ? _sumBuyingSpent + spent
+        : _sumBuyingSpent
+      const sumBuyingAmount = isBuying
+        ? _sumBuyingAmount + execAmount
+        : _sumBuyingAmount
+      const sumSellingSpent = (
+        !isBuying &&
+        Number.isFinite(spent)
+      )
+        ? _sumSellingSpent + spent
+        : _sumSellingSpent
+      const sumSellingAmount = !isBuying
+        ? _sumSellingAmount + execAmount
+        : _sumSellingAmount
+
+      symbResMap.set(symbol, {
+        sumSpent,
+        sumAmount,
+        sumBuyingSpent,
+        sumBuyingAmount,
+        sumSellingSpent,
+        sumSellingAmount,
+
+        buyingWeightedPrice: sumBuyingAmount === 0
+          ? 0
+          : sumBuyingSpent / sumBuyingAmount,
+        buyingAmount: sumBuyingAmount,
+        sellingWeightedPrice: sumSellingAmount === 0
+          ? 0
+          : sumSellingSpent / sumSellingAmount,
+        sellingAmount: sumSellingAmount,
+        cumulativeWeightedPrice: sumAmount === 0
+          ? 0
+          : sumSpent / sumAmount,
+        cumulativeAmount: sumAmount
+      })
+    }
+
+    return [...symbResMap].map(([symbol, val]) => {
+      const {
+        buyingWeightedPrice = 0,
+        buyingAmount = 0,
+        sellingWeightedPrice = 0,
+        sellingAmount = 0,
+        cumulativeWeightedPrice = 0,
+        cumulativeAmount = 0
+      } = val ?? {}
+
+      return {
+        symbol,
+        buyingWeightedPrice,
+        buyingAmount,
+        sellingWeightedPrice,
+        sellingAmount,
+        cumulativeWeightedPrice,
+        cumulativeAmount
+      }
+    })
   }
 }
 
