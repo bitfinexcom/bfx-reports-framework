ac0e464d12229721d0003830cd4f8bab1b07936c
Vladimir Voronkov <vsvoronkov@gmail.com>
2023-02-07 13:13:07 +0200

    Add v32 db migration for step down

--- workers/loc.api/sync/dao/db-migrations/sqlite-migrations/migration.v32.js
+++ workers/loc.api/sync/dao/db-migrations/sqlite-migrations/migration.v32.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const AbstractMigration = require('./abstract.migration')
+const { getSqlArrToModifyColumns } = require('./helpers')
 
 class MigrationV32 extends AbstractMigration {
   /**
@@ -21,7 +22,28 @@ class MigrationV32 extends AbstractMigration {
    * @override
    */
   down () {
-    const sqlArr = []
+    const sqlArr = [
+      ...getSqlArrToModifyColumns(
+        'users',
+        {
+          _id: 'INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT',
+          id: 'BIGINT',
+          email: 'VARCHAR(255)',
+          apiKey: 'VARCHAR(255) NOT NULL',
+          apiSecret: 'VARCHAR(255) NOT NULL',
+          active: 'INT',
+          isDataFromDb: 'INT',
+          timezone: 'VARCHAR(255)',
+          username: 'VARCHAR(255)',
+          passwordHash: 'VARCHAR(255)',
+          isNotProtected: 'INT',
+          isSubAccount: 'INT',
+          isSubUser: 'INT',
+          createdAt: 'BIGINT',
+          updatedAt: 'BIGINT'
+        }
+      )
+    ]
 
     this.addSql(sqlArr)
   }
