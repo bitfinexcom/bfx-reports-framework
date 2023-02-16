3bf962ce4b88fb42e1f3f3793d7d0e4d1a76aeb2
Vladimir Voronkov <vsvoronkov@gmail.com>
2023-02-16 14:16:50 +0200

    Mock generate_token endpoint for bfx rest v2

--- test/helpers/mock-data.js
+++ test/helpers/mock-data.js
@@ -7,6 +7,10 @@ const _ms = Date.now()
 module.exports = new Map([
   ...mockData,
   [
+    'generate_token',
+    ['pub:api:88888888-4444-4444-4444-121212121212-caps:s:o:f:w:wd:a-write']
+  ],
+  [
     'candles',
     [[
       _ms,
