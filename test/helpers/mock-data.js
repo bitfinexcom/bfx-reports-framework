759c7f57c8df145f0272929e390991ef2f212328
Vladimir Voronkov <vsvoronkov@gmail.com>
2023-03-23 11:56:28 +0200

    Add login mock data

--- test/helpers/mock-data.js
+++ test/helpers/mock-data.js
@@ -7,6 +7,25 @@ const _ms = Date.now()
 module.exports = new Map([
   ...mockData,
   [
+    'login',
+    [
+      '12345678-8888-4321-1234-8cb090a01360',
+      [
+        [
+          'otp',
+          true
+        ]
+      ]
+    ]
+  ],
+  [
+    'login_verify',
+    [
+      'pub:api:88888888-4444-4444-4444-121212121212-caps:s:o:f:w:wd:a-write',
+      '2023-03-21T12:00:00.000Z'
+    ]
+  ],
+  [
     'generate_token',
     ['pub:api:88888888-4444-4444-4444-121212121212-caps:s:o:f:w:wd:a-write']
   ],
