eb3d4cbb8d2f2ddd70ada9cb8ae6f3c4e19a9a97
Vladimir Voronkov <vsvoronkov@gmail.com>
2023-04-04 13:30:53 +0300

    Add v34 db migration

---
+++ workers/loc.api/sync/dao/db-migrations/sqlite-migrations/migration.v34.js
@@ -0,0 +1,66 @@
+'use strict'
+
+const AbstractMigration = require('./abstract.migration')
+const { getSqlArrToModifyColumns } = require('./helpers')
+
+const {
+  ID_PRIMARY_KEY
+} = require('./helpers/const')
+
+class MigrationV34 extends AbstractMigration {
+  /**
+   * @override
+   */
+  up () {
+    const sqlArr = [
+      'ALTER TABLE users ADD COLUMN authTokenTTLSec INT'
+    ]
+
+    this.addSql(sqlArr)
+  }
+
+  /**
+   * @override
+   */
+  beforeDown () { return this.dao.disableForeignKeys() }
+
+  /**
+   * @override
+   */
+  down () {
+    const sqlArr = [
+      ...getSqlArrToModifyColumns(
+        'users',
+        {
+          _id: ID_PRIMARY_KEY,
+          id: 'BIGINT',
+          email: 'VARCHAR(255)',
+          apiKey: 'VARCHAR(255)',
+          apiSecret: 'VARCHAR(255)',
+          authToken: 'VARCHAR(255)',
+          active: 'INT',
+          isDataFromDb: 'INT',
+          timezone: 'VARCHAR(255)',
+          username: 'VARCHAR(255)',
+          passwordHash: 'VARCHAR(255)',
+          isNotProtected: 'INT',
+          isSubAccount: 'INT',
+          isSubUser: 'INT',
+          shouldNotSyncOnStartupAfterUpdate: 'INT',
+          isSyncOnStartupRequired: 'INT',
+          createdAt: 'BIGINT',
+          updatedAt: 'BIGINT'
+        }
+      )
+    ]
+
+    this.addSql(sqlArr)
+  }
+
+  /**
+   * @override
+   */
+  afterDown () { return this.dao.enableForeignKeys() }
+}
+
+module.exports = MigrationV34
