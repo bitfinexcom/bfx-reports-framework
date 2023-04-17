647402ff4cdcb1645052a573a47ff403ed92207b
Vladimir Voronkov <vsvoronkov@gmail.com>
2023-04-17 15:22:30 +0300

    Add ability to make new migration files by template

--- scripts/node/migration/cmds/add.js
+++ scripts/node/migration/cmds/add.js
@@ -1,8 +1,41 @@
 'use strict'
 
+const path = require('path')
+const fs = require('fs')
+
 const {
   SUPPORTED_DB_VERSION
 } = require('../../../../workers/loc.api/sync/schema')
+
+const migrationFolderPath = path.join(
+  __dirname,
+  '../../../../workers/loc.api/sync/dao/db-migrations/sqlite-migrations'
+)
+const migrationTemplate = fs.readFileSync(
+  path.join(__dirname, 'migration-template.js.temp'),
+  'utf8'
+)
+const placeholderPattern = /#\{[a-zA-Z0-9]+\}/g
+
+const _renderMigrationTemplate = (
+  template,
+  params = {}
+) => {
+  const str = template.replace(placeholderPattern, (match) => {
+    const propName = match.replace('#{', '').replace('}', '')
+
+    if (
+      !Number.isFinite(params?.[propName]) &&
+      typeof params?.[propName] !== 'string'
+    ) {
+      return ''
+    }
+
+    return params[propName]
+  })
+
+  return str
+}
 
 module.exports = {
   command: 'add [version]',
@@ -23,12 +56,56 @@ module.exports = {
       })
   },
 
-  // TODO:
   handler: (argv) => {
+    const mts = Date.now()
+    const timestamp = new Date(mts).toISOString()
     const version = argv.next
       ? SUPPORTED_DB_VERSION + 1
       : argv.ver
 
-    console.log('Added a new DB migration file:'.blue, `v${version}`.bgGreen, '\n')
+    const migrationBody = _renderMigrationTemplate(
+      migrationTemplate,
+      { timestamp, version }
+    )
+    const migrationFileName = `migration.v${version}.${mts}.js`
+    const migrationFilePath = path.join(
+      migrationFolderPath,
+      migrationFileName
+    )
+
+    const migrationFileDirents = fs.readdirSync(
+      migrationFolderPath,
+      { withFileTypes: true }
+    )
+
+    const currMigrationVersionDirent = migrationFileDirents.find((dirent) => (
+      dirent.isFile() &&
+      new RegExp(`^migration.v${version}.\\d+.js$`).test(dirent.name)
+    ))
+
+    if (currMigrationVersionDirent?.name) {
+      console.log(
+        `V${version} DB migration file already exists, file name:`.yellow,
+        currMigrationVersionDirent.name.bgBlue,
+        '\n'
+      )
+
+      return
+    }
+
+    fs.writeFileSync(
+      migrationFilePath,
+      migrationBody,
+      {
+        encoding: 'utf8',
+        mode: '664'
+      }
+    )
+
+    console.log(
+      'Added a new DB migration file:'.blue,
+      `${migrationFileName}`.bgGreen,
+      '\n'
+    )
   }
 }
