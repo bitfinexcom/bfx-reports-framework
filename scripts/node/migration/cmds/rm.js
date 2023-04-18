2c66bb944e21d5ad0b429030cdf8f93b546831e7
Vladimir Voronkov <vsvoronkov@gmail.com>
2023-04-18 15:05:55 +0300

    Add ability to remove redundant db migration files

--- scripts/node/migration/cmds/rm.js
+++ scripts/node/migration/cmds/rm.js
@@ -1,8 +1,22 @@
 'use strict'
 
+const path = require('path')
+const fs = require('fs')
+const moment = require('moment')
+const { orderBy } = require('lodash')
+
 const {
   SUPPORTED_DB_VERSION
 } = require('../../../../workers/loc.api/sync/schema')
+
+const migrationFolderPath = path.join(
+  __dirname,
+  '../../../../workers/loc.api/sync/dao/db-migrations/sqlite-migrations'
+)
+const trashFolderPath = path.join(
+  migrationFolderPath,
+  'trash'
+)
 
 module.exports = {
   command: 'rm [totally]',
@@ -17,22 +31,97 @@ module.exports = {
       })
   },
 
-  // TODO:
   handler: (argv) => {
     const shouldBeRemoved = argv.totally
-    const fileNames = [] // TODO:
-    const strFileNames = fileNames.reduce((accum, name) => (
+    const mts = Date.now()
+    const momentMts = moment.utc(mts)
+    const maxAllowedMts = momentMts.subtract(6, 'months')
+      .valueOf()
+
+    const migrationFileDirents = fs.readdirSync(
+      migrationFolderPath,
+      { withFileTypes: true }
+    )
+    const migrationFileMetadata = migrationFileDirents
+      .reduce((accum, dirent) => {
+        if (
+          !dirent.isFile() ||
+          !/^migration.v\d+.js$/.test(dirent.name)
+        ) {
+          return accum
+        }
+
+        const splitName = dirent.name.split('.')
+        const version = Number.parseInt(splitName[1].replace('v', ''))
+        const mts = Number.parseInt(splitName[2] ?? 0)
+
+        if (
+          Number.isNaN(version) ||
+          Number.isNaN(mts)
+        ) {
+          return accum
+        }
+
+        accum.push({
+          name: dirent.name,
+          version,
+          mts
+        })
+
+        return accum
+      }, [])
+    const orderedMigrationFileMetadata = orderBy(
+      migrationFileMetadata,
+      ['version', 'mts'],
+      ['asc', 'asc']
+    )
+
+    const removingFileNames = []
+    const removingFilePaths = []
+
+    for (const metadata of orderedMigrationFileMetadata) {
+      const nonRemovableFilesAmount = (orderedMigrationFileMetadata.length -
+        removingFileNames.length)
+
+      if (
+        metadata.version === SUPPORTED_DB_VERSION ||
+        nonRemovableFilesAmount <= 10 ||
+        maxAllowedMts <= metadata.mts
+      ) {
+        break
+      }
+
+      removingFileNames.push(metadata.name)
+      removingFilePaths.push(path.join(
+        migrationFolderPath,
+        metadata.name
+      ))
+    }
+
+    const strRemovingFileNames = removingFileNames.reduce((accum, name) => (
       `${accum}  - ${name}\n`
     ), '')
 
     if (shouldBeRemoved) {
+      for (const filePaths of removingFilePaths) {
+        fs.rmSync(filePaths, { maxRetries: 10, recursive: true })
+      }
+
       console.log('Remover redundant migration files:'.blue)
-      console.log(strFileNames.red)
+      console.log(strRemovingFileNames.red)
 
       return
     }
 
+    fs.mkdirSync(trashFolderPath, { recursive: true })
+    for (const [i, filePaths] of removingFilePaths.entries()) {
+      fs.renameSync(
+        filePaths,
+        path.join(trashFolderPath, removingFileNames[i])
+      )
+    }
+
     console.log('Moved redundant migration files to the trash folder:'.blue)
-    console.log(strFileNames.yellow)
+    console.log(strRemovingFileNames.yellow)
   }
 }
