94343f655af8aaf38567a755f2ee13b1e84a6f2d
Vladimir Voronkov <vsvoronkov@gmail.com>
2024-01-26 11:26:59 +0200

    Extend templates and translation in extended pdf writer

--- workers/loc.api/generate-report-file/pdf-writer/index.js
+++ workers/loc.api/generate-report-file/pdf-writer/index.js
@@ -1,18 +1,28 @@
 'use strict'
 
+const path = require('path')
 const pdf = require('html-pdf')
 
 const MainPdfWriter = require(
   'bfx-report/workers/loc.api/generate-report-file/pdf-writer'
 )
 
-const { decorateInjectable } = require('../../di/utils')
+const TEMPLATE_FILE_NAMES = require('./template-file-names')
 
-const depsTypes = (TYPES) => [
-  TYPES.HasGrcService,
-  TYPES.GrcBfxReq
-]
 class PdfWriter extends MainPdfWriter {
+  constructor (...deps) {
+    super(...deps)
+
+    this.addTranslations(this.loadTranslations(
+      path.join(__dirname, 'translations.yml')
+    ))
+    this.addTemplates({
+      fileNames: TEMPLATE_FILE_NAMES,
+      templateFolderPath: path.join(__dirname, 'templates')
+    })
+    this.compileTemplate()
+  }
+
   /**
    * @override
    */
@@ -38,7 +48,5 @@ class PdfWriter extends MainPdfWriter {
     })
   }
 }
-
-decorateInjectable(PdfWriter, depsTypes)
 
 module.exports = PdfWriter
