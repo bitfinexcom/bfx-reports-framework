22cfede232016351690bc72ac56f45da833d8131
Vladimir Voronkov <vsvoronkov@gmail.com>
2024-04-16 12:30:39 +0300

    Add test coverage for lifo lookup buy trx with unrealized profit

--- workers/loc.api/sync/transaction.tax.report/helpers/__test__/look-up-trades.spec.js
+++ workers/loc.api/sync/transaction.tax.report/helpers/__test__/look-up-trades.spec.js
@@ -8,6 +8,9 @@ const {
   mockTrades,
   getMockedTrades
 } = require('./helpers')
+const {
+  testBuyTradesWithUnrealizedProfit
+} = require('./test-cases')
 
 describe('lookUpTrades helper for trx tax report', () => {
   it('Lookup buy trx with unrealized profit, LIFO strategy', async function () {
@@ -25,7 +28,54 @@ describe('lookUpTrades helper for trx tax report', () => {
       }
     )
 
-    console.log('[buyTradesWithUnrealizedProfit]:', buyTradesWithUnrealizedProfit)
+    assert.isArray(buyTradesWithUnrealizedProfit)
+    assert.equal(buyTradesWithUnrealizedProfit.length, 5)
+
+    testBuyTradesWithUnrealizedProfit(buyTradesWithUnrealizedProfit, 0, {
+      isMovements: false,
+      mtsCreate: Date.UTC(2023, 5, 11),
+      firstSymb: 'ETH',
+      lastSymb: 'UST',
+      execAmount: -1,
+      execPrice: 2_800,
+      buyFilledAmount: 100
+    })
+    testBuyTradesWithUnrealizedProfit(buyTradesWithUnrealizedProfit, 1, {
+      isMovements: false,
+      mtsCreate: Date.UTC(2023, 4, 22),
+      firstSymb: 'ETH',
+      lastSymb: 'BTC',
+      execAmount: -1,
+      execPrice: 0.055,
+      buyFilledAmount: 0
+    })
+    testBuyTradesWithUnrealizedProfit(buyTradesWithUnrealizedProfit, 2, {
+      isMovements: false,
+      mtsCreate: Date.UTC(2023, 2, 23),
+      firstSymb: 'ETH',
+      lastSymb: 'BTC',
+      execAmount: 10,
+      execPrice: 0.05,
+      buyFilledAmount: 7
+    })
+    testBuyTradesWithUnrealizedProfit(buyTradesWithUnrealizedProfit, 3, {
+      isMovements: true,
+      mtsCreate: Date.UTC(2023, 1, 5),
+      firstSymb: 'BTC',
+      lastSymb: 'USD',
+      execAmount: 20,
+      execPrice: 43_000,
+      buyFilledAmount: 2.5
+    })
+    testBuyTradesWithUnrealizedProfit(buyTradesWithUnrealizedProfit, 4, {
+      isMovements: false,
+      mtsCreate: Date.UTC(2023, 0, 10),
+      firstSymb: 'BTC',
+      lastSymb: 'USD',
+      execAmount: 3,
+      execPrice: 20_000,
+      buyFilledAmount: 0
+    })
   })
 
   it('Lookup sale trx with realized profit, LIFO strategy', async function () {
