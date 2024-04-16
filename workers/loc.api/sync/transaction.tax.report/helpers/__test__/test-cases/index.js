53ea82b206a4b63f70f43358c8a48a8c64a7ecac
Vladimir Voronkov <vsvoronkov@gmail.com>
2024-04-16 12:28:51 +0300

    Add test case checker for buy trades with unrealized profit

---
+++ workers/loc.api/sync/transaction.tax.report/helpers/__test__/test-cases/index.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const testBuyTradesWithUnrealizedProfit = require('./test-buy-trades-with-unrealized-profit')
+
+module.exports = {
+  testBuyTradesWithUnrealizedProfit
+}
---
+++ workers/loc.api/sync/transaction.tax.report/helpers/__test__/test-cases/test-buy-trades-with-unrealized-profit.js
@@ -0,0 +1,48 @@
+'use strict'
+
+const { assert } = require('chai')
+
+module.exports = (arr, index, props) => {
+  const trade = arr[index]
+  const {
+    isMovements,
+    mtsCreate,
+    firstSymb,
+    lastSymb,
+    execAmount,
+    execPrice,
+
+    buyFilledAmount
+  } = props ?? {}
+
+  assert.isObject(trade)
+  assert.isBoolean(trade.isBuyTradesWithUnrealizedProfitForPrevPeriod)
+  assert.isOk(trade.isBuyTradesWithUnrealizedProfitForPrevPeriod)
+  assert.isBoolean(trade.isBuyTrx)
+  assert.isOk(trade.isBuyTrx)
+  assert.isBoolean(trade.isBuyTrxHistFilled)
+  assert.isNotOk(trade.isBuyTrxHistFilled)
+  assert.isNumber(trade.proceedsForBuyTrx)
+  assert.equal(trade.proceedsForBuyTrx, 0)
+  assert.isNumber(trade.proceedsForBuyTrx)
+  assert.equal(trade.proceedsForBuyTrx, 0)
+  assert.isNumber(trade.firstSymbPrise)
+  assert.isNumber(trade.lastSymbPrise)
+  assert.isArray(trade.saleTrxsForRealizedProfit)
+
+  assert.isBoolean(trade.isMovements)
+  assert.equal(trade.isMovements, isMovements)
+  assert.isNumber(trade.mtsCreate)
+  assert.equal(trade.mtsCreate, mtsCreate)
+  assert.isString(trade.firstSymb)
+  assert.equal(trade.firstSymb, firstSymb)
+  assert.isString(trade.lastSymb)
+  assert.equal(trade.lastSymb, lastSymb)
+  assert.isNumber(trade.execAmount)
+  assert.equal(trade.execAmount, execAmount)
+  assert.isNumber(trade.execPrice)
+  assert.equal(trade.execPrice, execPrice)
+
+  assert.isNumber(trade.buyFilledAmount)
+  assert.equal(trade.buyFilledAmount, buyFilledAmount)
+}
