f56ce5df41c4adaea82b0cde0918f2bb0512e3c9
Vladimir Voronkov <vsvoronkov@gmail.com>
2024-04-17 13:52:51 +0300

    Add test coverage for lifo lookup sale trx with realized profit

--- workers/loc.api/sync/transaction.tax.report/helpers/__test__/look-up-trades.spec.js
+++ workers/loc.api/sync/transaction.tax.report/helpers/__test__/look-up-trades.spec.js
@@ -9,7 +9,8 @@ const {
   getMockedTrades
 } = require('./helpers')
 const {
-  testBuyTradesWithUnrealizedProfit
+  testBuyTradesWithUnrealizedProfit,
+  testSaleTradesWithRealizedProfit
 } = require('./test-cases')
 
 describe('lookUpTrades helper for trx tax report', () => {
@@ -89,7 +90,72 @@ describe('lookUpTrades helper for trx tax report', () => {
       }
     )
 
-    console.log('[saleTradesWithRealizedProfit]:', saleTradesWithRealizedProfit)
+    assert.isArray(saleTradesWithRealizedProfit)
+    assert.equal(saleTradesWithRealizedProfit.length, 7)
+
+    testSaleTradesWithRealizedProfit(saleTradesWithRealizedProfit, 0, {
+      asset: 'UST',
+      amount: 100,
+      mtsAcquired: Date.UTC(2023, 5, 11),
+      mtsSold: Date.UTC(2023, 6, 21),
+      proceeds: 105,
+      cost: 111,
+      gainOrLoss: -6
+    })
+    testSaleTradesWithRealizedProfit(saleTradesWithRealizedProfit, 1, {
+      asset: 'ETH',
+      amount: 1,
+      mtsAcquired: Date.UTC(2023, 2, 23),
+      mtsSold: Date.UTC(2023, 5, 11),
+      proceeds: 3110,
+      cost: 2601,
+      gainOrLoss: 509
+    })
+    testSaleTradesWithRealizedProfit(saleTradesWithRealizedProfit, 2, {
+      asset: 'ETH',
+      amount: 1,
+      mtsAcquired: Date.UTC(2023, 2, 23),
+      mtsSold: Date.UTC(2023, 4, 22),
+      proceeds: 2650,
+      cost: 2601,
+      gainOrLoss: 49
+    })
+    testSaleTradesWithRealizedProfit(saleTradesWithRealizedProfit, 3, {
+      asset: 'ETH',
+      amount: 1,
+      mtsAcquired: Date.UTC(2023, 2, 23),
+      mtsSold: Date.UTC(2023, 4, 10),
+      proceeds: 2000,
+      cost: 2601,
+      gainOrLoss: -601
+    })
+    testSaleTradesWithRealizedProfit(saleTradesWithRealizedProfit, 4, {
+      asset: 'ETH',
+      amount: 2,
+      mtsAcquired: Date.UTC(2023, 2, 23),
+      mtsSold: Date.UTC(2023, 3, 10),
+      proceeds: 6400,
+      cost: 5202,
+      gainOrLoss: 1198
+    })
+    testSaleTradesWithRealizedProfit(saleTradesWithRealizedProfit, 5, {
+      asset: 'BTC',
+      amount: 0.5,
+      mtsAcquired: Date.UTC(2023, 1, 5),
+      mtsSold: Date.UTC(2023, 2, 23),
+      proceeds: 25000,
+      cost: 21500,
+      gainOrLoss: 3500
+    })
+    testSaleTradesWithRealizedProfit(saleTradesWithRealizedProfit, 6, {
+      asset: 'BTC',
+      amount: 2,
+      mtsAcquired: Date.UTC(2023, 1, 5),
+      mtsSold: Date.UTC(2023, 2, 3),
+      proceeds: 66000,
+      cost: 86000,
+      gainOrLoss: -20000
+    })
   })
 
   it('Lookup buy trx with unrealized profit, FIFO strategy', async function () {
