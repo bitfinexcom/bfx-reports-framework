1384e464461b7f5707f20c39d8ec62590c71091d
Vladimir Voronkov <vsvoronkov@gmail.com>
2024-04-22 11:46:05 +0300

    Add test coverage for remap-movements helper

---
+++ workers/loc.api/sync/transaction.tax.report/helpers/__test__/remap-movements.spec.js
@@ -0,0 +1,60 @@
+'use strict'
+
+const { assert } = require('chai')
+
+const remapMovements = require('../remap-movements')
+const {
+  mockMovements,
+  getMockedMovements
+} = require('./helpers')
+const {
+  testRemappedMovements
+} = require('./test-cases')
+
+describe('reMapMovements helper for trx tax report', () => {
+  it('Re-map movements to trade data structure', function () {
+    const remappedTrxs = []
+    const remappedTrxsForConvToUsd = []
+    const params = { remappedTrxs, remappedTrxsForConvToUsd }
+
+    const returnedParams = remapMovements(
+      getMockedMovements(mockMovements),
+      params
+    )
+
+    assert.isObject(returnedParams)
+    assert.equal(returnedParams, params)
+    assert.isArray(returnedParams.remappedTrxs)
+    assert.equal(returnedParams.remappedTrxs, remappedTrxs)
+    assert.isArray(returnedParams.remappedTrxsForConvToUsd)
+    assert.equal(returnedParams.remappedTrxsForConvToUsd, remappedTrxsForConvToUsd)
+
+    assert.equal(remappedTrxs.length, 4)
+    assert.equal(remappedTrxsForConvToUsd.length, remappedTrxs.length)
+
+    testRemappedMovements(remappedTrxs, 0, {
+      mtsCreate: Date.UTC(2023, 8, 29),
+      symbol: 'tBTCUSD',
+      firstSymb: 'BTC',
+      execAmount: -0.9
+    })
+    testRemappedMovements(remappedTrxs, 1, {
+      mtsCreate: Date.UTC(2023, 5, 12),
+      symbol: 'tETHUSD',
+      firstSymb: 'ETH',
+      execAmount: 19.6
+    })
+    testRemappedMovements(remappedTrxs, 2, {
+      mtsCreate: Date.UTC(2023, 3, 21),
+      symbol: 'tBTCUSD',
+      firstSymb: 'BTC',
+      execAmount: 2.4
+    })
+    testRemappedMovements(remappedTrxs, 3, {
+      mtsCreate: Date.UTC(2023, 1, 15),
+      symbol: 'tUSTUSD',
+      firstSymb: 'UST',
+      execAmount: 301
+    })
+  })
+})
