ebee5072739328816339b870d841ad5f9256c255
Vladimir Voronkov <vsvoronkov@gmail.com>
2024-04-22 10:55:20 +0300

    Add mock movements

---
+++ workers/loc.api/sync/transaction.tax.report/helpers/__test__/helpers/mock-movements.js
@@ -0,0 +1,49 @@
+'use strict'
+
+module.exports = [
+  {
+    currency: 'BTC',
+    mtsUpdated: Date.UTC(2023, 8, 29),
+    amount: -0.9
+  },
+  {
+    currency: 'USD',
+    mtsUpdated: Date.UTC(2023, 7, 17),
+    amount: -123
+  },
+  {
+    currency: 'JPY',
+    mtsUpdated: Date.UTC(2023, 6, 19),
+    amount: 111.2
+  },
+  {
+    currency: 'ETH',
+    mtsUpdated: Date.UTC(2023, 5, 12),
+    amount: 19.6
+  },
+  {
+    currency: 'GBP',
+    mtsUpdated: Date.UTC(2023, 4, 3),
+    amount: 602
+  },
+  {
+    currency: 'BTC',
+    mtsUpdated: Date.UTC(2023, 3, 21),
+    amount: 2.4
+  },
+  {
+    currency: 'EUR',
+    mtsUpdated: Date.UTC(2023, 2, 17),
+    amount: 210
+  },
+  {
+    currency: 'UST',
+    mtsUpdated: Date.UTC(2023, 1, 15),
+    amount: 301
+  },
+  {
+    currency: 'USD',
+    mtsUpdated: Date.UTC(2023, 0, 11),
+    amount: 200
+  }
+]
