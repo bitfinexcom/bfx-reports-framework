15019ae6b076dc2788ef96b2f3df84c73a5bf71b
Vladimir Voronkov <vsvoronkov@gmail.com>
2024-05-07 09:31:22 +0300

    Fix converting symbols like tETHF0:USTF0 for trx tax report

--- workers/loc.api/sync/transaction.tax.report/helpers/get-trx-map-by-ccy.js
+++ workers/loc.api/sync/transaction.tax.report/helpers/get-trx-map-by-ccy.js
@@ -4,19 +4,24 @@ const {
   isForexSymb
 } = require('../../helpers')
 
+// Handle tETHF0:USTF0 symbols
+const symbRegExpNormalizer = /F0$/i
+
 module.exports = (trxs) => {
   const trxMapByCcy = new Map()
 
   for (const trx of trxs) {
+    const firstSymb = trx.firstSymb.replace(symbRegExpNormalizer, '')
+    const lastSymb = trx.lastSymb.replace(symbRegExpNormalizer, '')
     const isNotFirstSymbForex = !isForexSymb(trx.firstSymb)
     const isNotLastSymbForex = !isForexSymb(trx.lastSymb)
 
     if (isNotFirstSymbForex) {
-      if (!trxMapByCcy.has(trx.firstSymb)) {
-        trxMapByCcy.set(trx.firstSymb, [])
+      if (!trxMapByCcy.has(firstSymb)) {
+        trxMapByCcy.set(firstSymb, [])
       }
 
-      trxMapByCcy.get(trx.firstSymb).push({
+      trxMapByCcy.get(firstSymb).push({
         isNotFirstSymbForex,
         isNotLastSymbForex,
         mainPricePropName: 'firstSymbPrice',
@@ -25,11 +30,11 @@ module.exports = (trxs) => {
       })
     }
     if (isNotLastSymbForex) {
-      if (!trxMapByCcy.has(trx.lastSymb)) {
-        trxMapByCcy.set(trx.lastSymb, [])
+      if (!trxMapByCcy.has(lastSymb)) {
+        trxMapByCcy.set(lastSymb, [])
       }
 
-      trxMapByCcy.get(trx.lastSymb).push({
+      trxMapByCcy.get(lastSymb).push({
         isNotFirstSymbForex,
         isNotLastSymbForex,
         mainPricePropName: 'lastSymbPrice',
