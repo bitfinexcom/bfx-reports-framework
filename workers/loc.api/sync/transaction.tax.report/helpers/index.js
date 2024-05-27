7f12b5b211170e84e5d606e6abad61812c4a52bb
Vladimir Voronkov <vsvoronkov@gmail.com>
2024-05-27 07:30:06 +0300

    Add core fn for lookup trx with realized or unrealized profit

--- workers/loc.api/sync/transaction.tax.report/helpers/index.js
+++ workers/loc.api/sync/transaction.tax.report/helpers/index.js
@@ -3,9 +3,11 @@
 const TRX_TAX_STRATEGIES = require('./trx.tax.strategies')
 const remapTrades = require('./remap-trades')
 const remapMovements = require('./remap-movements')
+const lookUpTrades = require('./look-up-trades')
 
 module.exports = {
   TRX_TAX_STRATEGIES,
   remapTrades,
-  remapMovements
+  remapMovements,
+  lookUpTrades
 }
