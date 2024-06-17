f3bc3ae335c582d4e85b1df42995b9473c89c5bf
Vladimir Voronkov <vsvoronkov@gmail.com>
2024-06-17 10:00:58 +0300

    Add priority currency list

--- workers/loc.api/sync/transaction.tax.report/helpers/index.js
+++ workers/loc.api/sync/transaction.tax.report/helpers/index.js
@@ -1,12 +1,14 @@
 'use strict'
 
 const TRX_TAX_STRATEGIES = require('./trx.tax.strategies')
+const PRIORITY_CURRENCY_LIST = require('./priority.currency.list')
 const remapTrades = require('./remap-trades')
 const remapMovements = require('./remap-movements')
 const lookUpTrades = require('./look-up-trades')
 
 module.exports = {
   TRX_TAX_STRATEGIES,
+  PRIORITY_CURRENCY_LIST,
   remapTrades,
   remapMovements,
   lookUpTrades
---
+++ workers/loc.api/sync/transaction.tax.report/helpers/priority.currency.list.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const PRIORITY_CURRENCY_LIST = [
+  'BTC',
+  'ETH',
+  'UST'
+]
+
+module.exports = PRIORITY_CURRENCY_LIST
