3ad7e1df4549841f6d89b941ccce17ece6f11dea
Vladimir Voronkov <vsvoronkov@gmail.com>
2024-06-27 12:11:32 +0300

    Add test case to test sale trades with realized profit

--- workers/loc.api/sync/transaction.tax.report/helpers/__test__/test-cases/index.js
+++ workers/loc.api/sync/transaction.tax.report/helpers/__test__/test-cases/index.js
@@ -3,7 +3,11 @@
 const testBuyTradesWithUnrealizedProfit = require(
   './test-buy-trades-with-unrealized-profit'
 )
+const testSaleTradesWithRealizedProfit = require(
+  './test-sale-trades-with-realized-profit'
+)
 
 module.exports = {
-  testBuyTradesWithUnrealizedProfit
+  testBuyTradesWithUnrealizedProfit,
+  testSaleTradesWithRealizedProfit
 }
