2efc033fb684c32eabf6e3785d59d5475176aca0
Vladimir Voronkov <vsvoronkov@gmail.com>
2024-07-15 11:56:46 +0300

    Move ledgers model to separate file

--- workers/loc.api/sync/schema/models/index.js
+++ workers/loc.api/sync/schema/models/index.js
@@ -13,6 +13,7 @@ const SUPPORTED_DB_VERSION = 41
 const TABLES_NAMES = require('../tables-names')
 const users = require('./users')
 const subAccounts = require('./sub-accounts')
+const ledgers = require('./ledgers')
 
 const {
   CONSTR_FIELD_NAME,
@@ -56,55 +57,7 @@ const _models = new Map([
   ],
   [
     TABLES_NAMES.LEDGERS,
-    {
-      _id: ID_PRIMARY_KEY,
-      id: 'BIGINT',
-      currency: 'VARCHAR(255)',
-      mts: 'BIGINT',
-      amount: 'DECIMAL(22,12)',
-      amountUsd: 'DECIMAL(22,12)',
-      exactUsdValue: 'DECIMAL(22,12)',
-      balance: 'DECIMAL(22,12)',
-      _nativeBalance: 'DECIMAL(22,12)',
-      balanceUsd: 'DECIMAL(22,12)',
-      _nativeBalanceUsd: 'DECIMAL(22,12)',
-      description: 'TEXT',
-      wallet: 'VARCHAR(255)',
-      _category: 'INT',
-      _isMarginFundingPayment: 'INT',
-      _isAffiliateRebate: 'INT',
-      _isStakingPayments: 'INT',
-      _isSubAccountsTransfer: 'INT',
-      _isBalanceRecalced: 'INT',
-      _isInvoicePayOrder: 'INT',
-      _isAirdropOnWallet: 'INT',
-      subUserId: 'INT',
-      user_id: 'INT NOT NULL',
-
-      [UNIQUE_INDEX_FIELD_NAME]: ['id', 'user_id'],
-      [INDEX_FIELD_NAME]: [
-        ['user_id', 'wallet', 'currency', 'mts'],
-        ['user_id', 'wallet', 'mts'],
-        ['user_id', 'currency', 'mts'],
-        ['user_id', '_isMarginFundingPayment', 'mts'],
-        ['user_id', '_isAffiliateRebate', 'mts'],
-        ['user_id', '_isStakingPayments', 'mts'],
-        ['user_id', '_isSubAccountsTransfer', 'mts'],
-        ['user_id', '_category', 'mts'],
-        ['user_id', 'mts'],
-        ['currency', 'mts'],
-        ['_isInvoicePayOrder'],
-        ['_isAirdropOnWallet'],
-        ['user_id', 'subUserId', 'mts',
-          'WHERE subUserId IS NOT NULL'],
-        ['subUserId', 'mts', '_id',
-          'WHERE subUserId IS NOT NULL']
-      ],
-      [CONSTR_FIELD_NAME]: [
-        USER_ID_CONSTRAINT,
-        SUB_USER_ID_CONSTRAINT
-      ]
-    }
+    ledgers
   ],
   [
     TABLES_NAMES.TRADES,
