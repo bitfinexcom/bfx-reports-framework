8ba57b0dd5b5126861b4d007d70193bbf00d0c72
Vladimir Voronkov <vsvoronkov@gmail.com>
2024-07-15 14:08:59 +0300

    Move logins model to separate file

--- workers/loc.api/sync/schema/models/index.js
+++ workers/loc.api/sync/schema/models/index.js
@@ -24,6 +24,7 @@ const fundingLoanHistory = require('./funding-loan-history')
 const fundingCreditHistory = require('./funding-credit-history')
 const positionsHistory = require('./positions-history')
 const positionsSnapshot = require('./positions-snapshot')
+const logins = require('./logins')
 
 const {
   CONSTR_FIELD_NAME,
@@ -111,26 +112,7 @@ const _models = new Map([
   ],
   [
     TABLES_NAMES.LOGINS,
-    {
-      _id: ID_PRIMARY_KEY,
-      id: 'BIGINT',
-      time: 'BIGINT',
-      ip: 'VARCHAR(255)',
-      extraData: 'TEXT',
-      subUserId: 'INT',
-      user_id: 'INT NOT NULL',
-
-      [UNIQUE_INDEX_FIELD_NAME]: ['id', 'user_id'],
-      [INDEX_FIELD_NAME]: [
-        ['user_id', 'time'],
-        ['user_id', 'subUserId', 'time',
-          'WHERE subUserId IS NOT NULL']
-      ],
-      [CONSTR_FIELD_NAME]: [
-        USER_ID_CONSTRAINT,
-        SUB_USER_ID_CONSTRAINT
-      ]
-    }
+    logins
   ],
   [
     TABLES_NAMES.CHANGE_LOGS,
---
+++ workers/loc.api/sync/schema/models/logins.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const {
+  CONSTR_FIELD_NAME,
+  INDEX_FIELD_NAME,
+  UNIQUE_INDEX_FIELD_NAME,
+  ID_PRIMARY_KEY
+} = require('../const')
+const {
+  USER_ID_CONSTRAINT,
+  SUB_USER_ID_CONSTRAINT
+} = require('../common.constraints')
+
+module.exports = {
+  _id: ID_PRIMARY_KEY,
+  id: 'BIGINT',
+  time: 'BIGINT',
+  ip: 'VARCHAR(255)',
+  extraData: 'TEXT',
+  subUserId: 'INT',
+  user_id: 'INT NOT NULL',
+
+  [UNIQUE_INDEX_FIELD_NAME]: ['id', 'user_id'],
+  [INDEX_FIELD_NAME]: [
+    ['user_id', 'time'],
+    ['user_id', 'subUserId', 'time',
+      'WHERE subUserId IS NOT NULL']
+  ],
+  [CONSTR_FIELD_NAME]: [
+    USER_ID_CONSTRAINT,
+    SUB_USER_ID_CONSTRAINT
+  ]
+}
