fc91e4a79f7d0c5603cc0c3b5fe8e061d3157941
Vladimir Voronkov <vsvoronkov@gmail.com>
2024-07-15 12:26:32 +0300

    Move fundingOfferHistory model to separate file

--- workers/loc.api/sync/schema/models/index.js
+++ workers/loc.api/sync/schema/models/index.js
@@ -19,6 +19,7 @@ const fundingTrades = require('./funding-trades')
 const publicTrades = require('./public-trades')
 const orders = require('./orders')
 const movements = require('./movements')
+const fundingOfferHistory = require('./funding-offer-history')
 
 const {
   CONSTR_FIELD_NAME,
@@ -86,40 +87,7 @@ const _models = new Map([
   ],
   [
     TABLES_NAMES.FUNDING_OFFER_HISTORY,
-    {
-      _id: ID_PRIMARY_KEY,
-      id: 'BIGINT',
-      symbol: 'VARCHAR(255)',
-      mtsCreate: 'BIGINT',
-      mtsUpdate: 'BIGINT',
-      amount: 'DECIMAL(22,12)',
-      amountOrig: 'DECIMAL(22,12)',
-      type: 'VARCHAR(255)',
-      flags: 'TEXT',
-      status: 'TEXT',
-      rate: 'VARCHAR(255)',
-      period: 'INT',
-      notify: 'INT',
-      hidden: 'INT',
-      renew: 'INT',
-      rateReal: 'INT',
-      amountExecuted: 'DECIMAL(22,12)',
-      subUserId: 'INT',
-      user_id: 'INT NOT NULL',
-
-      [UNIQUE_INDEX_FIELD_NAME]: ['id', 'user_id'],
-      [INDEX_FIELD_NAME]: [
-        ['user_id', 'symbol', 'mtsUpdate'],
-        ['user_id', 'status', 'mtsUpdate'],
-        ['user_id', 'mtsUpdate'],
-        ['user_id', 'subUserId', 'mtsUpdate',
-          'WHERE subUserId IS NOT NULL']
-      ],
-      [CONSTR_FIELD_NAME]: [
-        USER_ID_CONSTRAINT,
-        SUB_USER_ID_CONSTRAINT
-      ]
-    }
+    fundingOfferHistory
   ],
   [
     TABLES_NAMES.FUNDING_LOAN_HISTORY,
