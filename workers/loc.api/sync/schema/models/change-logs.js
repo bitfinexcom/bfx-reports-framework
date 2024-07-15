3260dcaf882df6e9422f59ac82ff62e87f2ca143
Vladimir Voronkov <vsvoronkov@gmail.com>
2024-07-15 14:11:29 +0300

    Move changeLogs model to separate file

---
+++ workers/loc.api/sync/schema/models/change-logs.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const {
+  CONSTR_FIELD_NAME,
+  INDEX_FIELD_NAME,
+  UNIQUE_INDEX_FIELD_NAME,
+  ID_PRIMARY_KEY
+} = require('../const')
+const {
+  USER_ID_CONSTRAINT,
+  SUB_USER_ID_CONSTRAINT
+} = require('../common.constraints')
+
+module.exports = {
+  _id: ID_PRIMARY_KEY,
+  mtsCreate: 'BIGINT',
+  log: 'VARCHAR(255)',
+  ip: 'VARCHAR(255)',
+  userAgent: 'TEXT',
+  subUserId: 'INT',
+  user_id: 'INT NOT NULL',
+
+  [UNIQUE_INDEX_FIELD_NAME]: ['mtsCreate', 'log', 'user_id'],
+  [INDEX_FIELD_NAME]: [
+    ['user_id', 'mtsCreate'],
+    ['user_id', 'subUserId', 'mtsCreate',
+      'WHERE subUserId IS NOT NULL']
+  ],
+  [CONSTR_FIELD_NAME]: [
+    USER_ID_CONSTRAINT,
+    SUB_USER_ID_CONSTRAINT
+  ]
+}
--- workers/loc.api/sync/schema/models/index.js
+++ workers/loc.api/sync/schema/models/index.js
@@ -25,6 +25,7 @@ const fundingCreditHistory = require('./funding-credit-history')
 const positionsHistory = require('./positions-history')
 const positionsSnapshot = require('./positions-snapshot')
 const logins = require('./logins')
+const changeLogs = require('./change-logs')
 
 const {
   CONSTR_FIELD_NAME,
@@ -116,26 +117,7 @@ const _models = new Map([
   ],
   [
     TABLES_NAMES.CHANGE_LOGS,
-    {
-      _id: ID_PRIMARY_KEY,
-      mtsCreate: 'BIGINT',
-      log: 'VARCHAR(255)',
-      ip: 'VARCHAR(255)',
-      userAgent: 'TEXT',
-      subUserId: 'INT',
-      user_id: 'INT NOT NULL',
-
-      [UNIQUE_INDEX_FIELD_NAME]: ['mtsCreate', 'log', 'user_id'],
-      [INDEX_FIELD_NAME]: [
-        ['user_id', 'mtsCreate'],
-        ['user_id', 'subUserId', 'mtsCreate',
-          'WHERE subUserId IS NOT NULL']
-      ],
-      [CONSTR_FIELD_NAME]: [
-        USER_ID_CONSTRAINT,
-        SUB_USER_ID_CONSTRAINT
-      ]
-    }
+    changeLogs
   ],
   [
     TABLES_NAMES.PAY_INVOICE_LIST,
