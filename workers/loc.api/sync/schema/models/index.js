2628ccc15e52da156dae893dd98067836ac29f7e
Vladimir Voronkov <vsvoronkov@gmail.com>
2024-07-15 12:02:36 +0300

    Move trades model to separate file

--- workers/loc.api/sync/schema/models/index.js
+++ workers/loc.api/sync/schema/models/index.js
@@ -14,6 +14,7 @@ const TABLES_NAMES = require('../tables-names')
 const users = require('./users')
 const subAccounts = require('./sub-accounts')
 const ledgers = require('./ledgers')
+const trades = require('./trades')
 
 const {
   CONSTR_FIELD_NAME,
@@ -61,40 +62,7 @@ const _models = new Map([
   ],
   [
     TABLES_NAMES.TRADES,
-    {
-      _id: ID_PRIMARY_KEY,
-      id: 'BIGINT',
-      symbol: 'VARCHAR(255)',
-      mtsCreate: 'BIGINT',
-      orderID: 'BIGINT',
-      execAmount: 'DECIMAL(22,12)',
-      execPrice: 'DECIMAL(22,12)',
-      exactUsdValue: 'DECIMAL(22,12)',
-      orderType: 'VARCHAR(255)',
-      orderPrice: 'DECIMAL(22,12)',
-      maker: 'INT',
-      fee: 'DECIMAL(22,12)',
-      feeCurrency: 'VARCHAR(255)',
-      subUserId: 'INT',
-      _isExchange: 'INT',
-      user_id: 'INT NOT NULL',
-
-      [UNIQUE_INDEX_FIELD_NAME]: ['id', 'symbol', 'user_id'],
-      [INDEX_FIELD_NAME]: [
-        ['user_id', 'symbol', 'mtsCreate'],
-        ['user_id', 'orderID', 'mtsCreate'],
-        ['user_id', '_isExchange', 'mtsCreate'],
-        ['user_id', 'mtsCreate'],
-        ['user_id', 'subUserId', 'mtsCreate',
-          'WHERE subUserId IS NOT NULL'],
-        ['subUserId', 'orderID',
-          'WHERE subUserId IS NOT NULL']
-      ],
-      [CONSTR_FIELD_NAME]: [
-        USER_ID_CONSTRAINT,
-        SUB_USER_ID_CONSTRAINT
-      ]
-    }
+    trades
   ],
   [
     TABLES_NAMES.FUNDING_TRADES,
