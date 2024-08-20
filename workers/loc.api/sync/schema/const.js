60a9f6c1a4d25a53b0d9ed97ad354fefbde12dad
Vladimir Voronkov <vsvoronkov@gmail.com>
2024-08-20 13:18:07 +0300

    Add uid field name into constants

--- workers/loc.api/sync/schema/const.js
+++ workers/loc.api/sync/schema/const.js
@@ -1,12 +1,16 @@
 'use strict'
 
+// TODO: need to move into model folder
+
+const UID_FIELD_NAME = '_id'
 const CONSTR_FIELD_NAME = '__constraints__'
 const TRIGGER_FIELD_NAME = '__triggers__'
 const INDEX_FIELD_NAME = '__indexies__'
 const UNIQUE_INDEX_FIELD_NAME = '__uniqueIndexies__'
-const ID_PRIMARY_KEY = 'INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT'
+const ID_PRIMARY_KEY = 'INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT' // TODO: need to remove
 
 module.exports = {
+  UID_FIELD_NAME,
   CONSTR_FIELD_NAME,
   TRIGGER_FIELD_NAME,
   INDEX_FIELD_NAME,
