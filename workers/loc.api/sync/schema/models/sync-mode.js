8d8689adce3dfc0af1551dac28693d13f451037c
Vladimir Voronkov <vsvoronkov@gmail.com>
2024-08-23 15:37:11 +0300

    Use Model class for sync-mode model

--- workers/loc.api/sync/schema/models/sync-mode.js
+++ workers/loc.api/sync/schema/models/sync-mode.js
@@ -1,18 +1,7 @@
 'use strict'
 
-const {
-  TRIGGER_FIELD_NAME,
-  ID_PRIMARY_KEY
-} = require('./model/db.service.field.names')
-const {
-  CREATE_UPDATE_MTS_TRIGGERS
-} = require('./model/common.triggers')
+const Model = require('./model')
 
-module.exports = {
-  _id: ID_PRIMARY_KEY,
-  isEnable: 'INT',
-  createdAt: 'BIGINT',
-  updatedAt: 'BIGINT',
-
-  [TRIGGER_FIELD_NAME]: CREATE_UPDATE_MTS_TRIGGERS
-}
+module.exports = new Model({
+  isEnable: Model.INTEGER
+}, { hasCreateUpdateMtsTriggers: true })
