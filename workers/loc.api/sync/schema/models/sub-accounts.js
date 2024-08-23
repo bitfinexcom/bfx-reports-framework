ec3943bcd6967403422abf5a38e57f80917c4e3c
Vladimir Voronkov <vsvoronkov@gmail.com>
2024-08-23 15:35:55 +0300

    Use Model class for sub-accounts model

--- workers/loc.api/sync/schema/models/sub-accounts.js
+++ workers/loc.api/sync/schema/models/sub-accounts.js
@@ -1,32 +1,15 @@
 'use strict'
 
-const {
-  CONSTR_FIELD_NAME,
-  TRIGGER_FIELD_NAME,
-  ID_PRIMARY_KEY
-} = require('./model/db.service.field.names')
-const {
-  CREATE_UPDATE_MTS_TRIGGERS,
-  DELETE_SUB_USERS_TRIGGER
-} = require('./model/common.triggers')
-const {
-  MASTER_USER_ID_CONSTRAINT,
-  SUB_USER_ID_CONSTRAINT
-} = require('./model/common.constraints')
+const Model = require('./model')
 
-module.exports = {
-  _id: ID_PRIMARY_KEY,
-  masterUserId: 'INT NOT NULL',
-  subUserId: 'INT NOT NULL',
-  createdAt: 'BIGINT',
-  updatedAt: 'BIGINT',
+module.exports = new Model({
+  masterUserId: Model.INTEGER_NOT_NULL,
+  subUserId: Model.INTEGER_NOT_NULL,
 
-  [CONSTR_FIELD_NAME]: [
-    MASTER_USER_ID_CONSTRAINT,
-    SUB_USER_ID_CONSTRAINT
+  [Model.CONSTR_FIELD_NAME]: [
+    Model.COMMON_CONSTRAINTS.MASTER_USER_ID_CONSTRAINT,
+    Model.COMMON_CONSTRAINTS.SUB_USER_ID_CONSTRAINT
   ],
-  [TRIGGER_FIELD_NAME]: [
-    DELETE_SUB_USERS_TRIGGER,
-    ...CREATE_UPDATE_MTS_TRIGGERS
-  ]
-}
+  [Model.TRIGGER_FIELD_NAME]: Model.COMMON_TRIGGERS
+    .DELETE_SUB_USERS_TRIGGER
+}, { hasCreateUpdateMtsTriggers: true })
