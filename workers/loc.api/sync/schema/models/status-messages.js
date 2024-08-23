1e494d58ff942ba6ff40fc03c032d04f20e2f0e5
Vladimir Voronkov <vsvoronkov@gmail.com>
2024-08-23 15:35:33 +0300

    Use Model class for status-messages model

--- workers/loc.api/sync/schema/models/status-messages.js
+++ workers/loc.api/sync/schema/models/status-messages.js
@@ -1,26 +1,21 @@
 'use strict'
 
-const {
-  INDEX_FIELD_NAME,
-  UNIQUE_INDEX_FIELD_NAME,
-  ID_PRIMARY_KEY
-} = require('./model/db.service.field.names')
+const Model = require('./model')
 
-module.exports = {
-  _id: ID_PRIMARY_KEY,
-  key: 'VARCHAR(255)',
-  timestamp: 'BIGINT',
-  price: 'DECIMAL(22,12)',
-  priceSpot: 'DECIMAL(22,12)',
-  fundBal: 'DECIMAL(22,12)',
-  fundingAccrued: 'DECIMAL(22,12)',
-  fundingStep: 'DECIMAL(22,12)',
-  clampMin: 'DECIMAL(22,12)',
-  clampMax: 'DECIMAL(22,12)',
-  _type: 'VARCHAR(255)',
+module.exports = new Model({
+  key: Model.VARCHAR,
+  timestamp: Model.BIGINT,
+  price: Model.DECIMAL,
+  priceSpot: Model.DECIMAL,
+  fundBal: Model.DECIMAL,
+  fundingAccrued: Model.DECIMAL,
+  fundingStep: Model.DECIMAL,
+  clampMin: Model.DECIMAL,
+  clampMax: Model.DECIMAL,
+  _type: Model.VARCHAR,
 
-  [UNIQUE_INDEX_FIELD_NAME]: ['key', '_type'],
-  [INDEX_FIELD_NAME]: [
+  [Model.UNIQUE_INDEX_FIELD_NAME]: ['key', '_type'],
+  [Model.INDEX_FIELD_NAME]: [
     ['key', 'timestamp']
   ]
-}
+})
