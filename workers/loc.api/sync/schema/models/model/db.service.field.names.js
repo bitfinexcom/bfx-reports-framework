9ae9551df13c095d1dfe511932f722466524b6b4
Vladimir Voronkov <vsvoronkov@gmail.com>
2024-08-23 15:42:39 +0300

    Remove redundant db service field

--- workers/loc.api/sync/schema/models/model/db.service.field.names.js
+++ workers/loc.api/sync/schema/models/model/db.service.field.names.js
@@ -5,13 +5,11 @@ const CONSTR_FIELD_NAME = '__constraints__'
 const TRIGGER_FIELD_NAME = '__triggers__'
 const INDEX_FIELD_NAME = '__indexies__'
 const UNIQUE_INDEX_FIELD_NAME = '__uniqueIndexies__'
-const ID_PRIMARY_KEY = 'INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT' // TODO: need to remove
 
 module.exports = {
   UID_FIELD_NAME,
   CONSTR_FIELD_NAME,
   TRIGGER_FIELD_NAME,
   INDEX_FIELD_NAME,
-  UNIQUE_INDEX_FIELD_NAME,
-  ID_PRIMARY_KEY
+  UNIQUE_INDEX_FIELD_NAME
 }
