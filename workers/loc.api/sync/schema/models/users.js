8def719d9c3d17fd3e775560ddb9e9d91d6cfef5
Vladimir Voronkov <vsvoronkov@gmail.com>
2024-08-23 15:39:09 +0300

    Use Model class for users model

--- workers/loc.api/sync/schema/models/users.js
+++ workers/loc.api/sync/schema/models/users.js
@@ -1,17 +1,8 @@
 'use strict'
 
-const {
-  TRIGGER_FIELD_NAME,
-  UNIQUE_INDEX_FIELD_NAME,
-  ID_PRIMARY_KEY
-} = require('./model/db.service.field.names')
-const {
-  CREATE_UPDATE_API_KEYS_TRIGGERS,
-  CREATE_UPDATE_MTS_TRIGGERS
-} = require('./model/common.triggers')
+const Model = require('./model')
 
-module.exports = {
-  _id: ID_PRIMARY_KEY,
+module.exports = new Model({
   id: 'BIGINT',
   email: 'VARCHAR(255)',
   apiKey: 'VARCHAR(255)',
@@ -30,12 +21,8 @@ module.exports = {
   isSyncOnStartupRequired: 'INT',
   authTokenTTLSec: 'INT',
   isStagingBfxApi: 'INT',
-  createdAt: 'BIGINT',
-  updatedAt: 'BIGINT',
 
-  [UNIQUE_INDEX_FIELD_NAME]: ['email', 'username'],
-  [TRIGGER_FIELD_NAME]: [
-    ...CREATE_UPDATE_API_KEYS_TRIGGERS,
-    ...CREATE_UPDATE_MTS_TRIGGERS
-  ]
-}
+  [Model.UNIQUE_INDEX_FIELD_NAME]: ['email', 'username'],
+  [Model.TRIGGER_FIELD_NAME]: Model.COMMON_TRIGGERS
+    .CREATE_UPDATE_API_KEYS_TRIGGERS
+}, { hasCreateUpdateMtsTriggers: true })
