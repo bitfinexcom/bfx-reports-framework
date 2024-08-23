9a4d74946a3f4f002e6c9844921ac82fe4a0ce36
Vladimir Voronkov <vsvoronkov@gmail.com>
2024-08-23 15:24:42 +0300

    Use Model class for candles model

--- workers/loc.api/sync/schema/models/candles.js
+++ workers/loc.api/sync/schema/models/candles.js
@@ -1,27 +1,22 @@
 'use strict'
 
-const {
-  INDEX_FIELD_NAME,
-  UNIQUE_INDEX_FIELD_NAME,
-  ID_PRIMARY_KEY
-} = require('./model/db.service.field.names')
+const Model = require('./model')
 
-module.exports = {
-  _id: ID_PRIMARY_KEY,
-  mts: 'BIGINT',
-  open: 'DECIMAL(22,12)',
-  close: 'DECIMAL(22,12)',
-  high: 'DECIMAL(22,12)',
-  low: 'DECIMAL(22,12)',
-  volume: 'DECIMAL(22,12)',
-  _symbol: 'VARCHAR(255)',
-  _timeframe: 'VARCHAR(255)',
+module.exports = new Model({
+  mts: Model.BIGINT,
+  open: Model.DECIMAL,
+  close: Model.DECIMAL,
+  high: Model.DECIMAL,
+  low: Model.DECIMAL,
+  volume: Model.DECIMAL,
+  _symbol: Model.VARCHAR,
+  _timeframe: Model.VARCHAR,
 
-  [UNIQUE_INDEX_FIELD_NAME]: ['_symbol', '_timeframe', 'mts'],
-  [INDEX_FIELD_NAME]: [
+  [Model.UNIQUE_INDEX_FIELD_NAME]: ['_symbol', '_timeframe', 'mts'],
+  [Model.INDEX_FIELD_NAME]: [
     ['_timeframe', '_symbol', 'mts'],
     ['_timeframe', 'mts'],
     ['_symbol', 'mts'],
     ['close', 'mts']
   ]
-}
+})
