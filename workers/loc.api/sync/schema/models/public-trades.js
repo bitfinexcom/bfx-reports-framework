80083f668248fdde845b0c0db5aed7037126e024
Vladimir Voronkov <vsvoronkov@gmail.com>
2024-08-23 15:34:43 +0300

    Use Model class for public-trades model

--- workers/loc.api/sync/schema/models/public-trades.js
+++ workers/loc.api/sync/schema/models/public-trades.js
@@ -1,24 +1,19 @@
 'use strict'
 
-const {
-  INDEX_FIELD_NAME,
-  UNIQUE_INDEX_FIELD_NAME,
-  ID_PRIMARY_KEY
-} = require('./model/db.service.field.names')
+const Model = require('./model')
 
-module.exports = {
-  _id: ID_PRIMARY_KEY,
-  id: 'BIGINT',
-  mts: 'BIGINT',
-  rate: 'DECIMAL(22,12)',
-  period: 'BIGINT',
-  amount: 'DECIMAL(22,12)',
-  price: 'DECIMAL(22,12)',
-  _symbol: 'VARCHAR(255)',
+module.exports = new Model({
+  id: Model.BIGINT,
+  mts: Model.BIGINT,
+  rate: Model.DECIMAL,
+  period: Model.BIGINT,
+  amount: Model.DECIMAL,
+  price: Model.DECIMAL,
+  _symbol: Model.VARCHAR,
 
-  [UNIQUE_INDEX_FIELD_NAME]: ['id', '_symbol'],
-  [INDEX_FIELD_NAME]: [
+  [Model.UNIQUE_INDEX_FIELD_NAME]: ['id', '_symbol'],
+  [Model.INDEX_FIELD_NAME]: [
     ['_symbol', 'mts'],
     ['mts']
   ]
-}
+})
