c38f1621e549b85a574411369b2167152719813f
Vladimir Voronkov <vsvoronkov@gmail.com>
2024-08-23 15:25:13 +0300

    Use Model class for change-logs model

--- workers/loc.api/sync/schema/models/change-logs.js
+++ workers/loc.api/sync/schema/models/change-logs.js
@@ -1,33 +1,23 @@
 'use strict'
 
-const {
-  CONSTR_FIELD_NAME,
-  INDEX_FIELD_NAME,
-  UNIQUE_INDEX_FIELD_NAME,
-  ID_PRIMARY_KEY
-} = require('./model/db.service.field.names')
-const {
-  USER_ID_CONSTRAINT,
-  SUB_USER_ID_CONSTRAINT
-} = require('./model/common.constraints')
+const Model = require('./model')
 
-module.exports = {
-  _id: ID_PRIMARY_KEY,
-  mtsCreate: 'BIGINT',
-  log: 'VARCHAR(255)',
-  ip: 'VARCHAR(255)',
-  userAgent: 'TEXT',
-  subUserId: 'INT',
-  user_id: 'INT NOT NULL',
+module.exports = new Model({
+  mtsCreate: Model.BIGINT,
+  log: Model.VARCHAR,
+  ip: Model.VARCHAR,
+  userAgent: Model.TEXT,
+  subUserId: Model.INTEGER,
+  user_id: Model.INTEGER_NOT_NULL,
 
-  [UNIQUE_INDEX_FIELD_NAME]: ['mtsCreate', 'log', 'user_id'],
-  [INDEX_FIELD_NAME]: [
+  [Model.UNIQUE_INDEX_FIELD_NAME]: ['mtsCreate', 'log', 'user_id'],
+  [Model.INDEX_FIELD_NAME]: [
     ['user_id', 'mtsCreate'],
     ['user_id', 'subUserId', 'mtsCreate',
       'WHERE subUserId IS NOT NULL']
   ],
-  [CONSTR_FIELD_NAME]: [
-    USER_ID_CONSTRAINT,
-    SUB_USER_ID_CONSTRAINT
+  [Model.CONSTR_FIELD_NAME]: [
+    Model.COMMON_CONSTRAINTS.USER_ID_CONSTRAINT,
+    Model.COMMON_CONSTRAINTS.SUB_USER_ID_CONSTRAINT
   ]
-}
+})
