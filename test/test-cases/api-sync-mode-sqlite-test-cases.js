ee1460060193d33ea0ab8e0e7f657abfb0df6d38
Vladimir Voronkov <vsvoronkov@gmail.com>
2024-08-29 11:09:16 +0300

    Add test coverage for getLastFinishedSyncMts method

--- test/test-cases/api-sync-mode-sqlite-test-cases.js
+++ test/test-cases/api-sync-mode-sqlite-test-cases.js
@@ -612,6 +612,26 @@ module.exports = (
     assert.isOk(Number.isInteger(res.body.result.lastSyncMts))
   })
 
+  it('it should be successfully performed by the getLastFinishedSyncMts method, lastSyncMts is integer', async function () {
+    this.timeout(5000)
+
+    const res = await agent
+      .post(`${basePath}/json-rpc`)
+      .type('json')
+      .send({
+        auth,
+        method: 'getLastFinishedSyncMts',
+        id: 5
+      })
+      .expect('Content-Type', /json/)
+      .expect(200)
+
+    assert.isObject(res.body)
+    assert.propertyVal(res.body, 'id', 5)
+    assert.isObject(res.body.result)
+    assert.isOk(Number.isInteger(res.body.result.lastSyncMts))
+  })
+
   it('it should be successfully performed by the haveCollsBeenSyncedAtLeastOnce method, returns true', async function () {
     this.timeout(60000)
 
