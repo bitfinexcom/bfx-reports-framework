a51d05c680702ee53cdd9a0a6adfcec25dfe9e54
Vladimir Voronkov <vsvoronkov@gmail.com>
2024-09-02 15:36:09 +0300

    Clone sync schema keeping initial model instance

--- workers/loc.api/sync/schema/helpers/index.js
+++ workers/loc.api/sync/schema/helpers/index.js
@@ -1,18 +1,24 @@
 'use strict'
 
 const { omit, cloneDeep } = require('lib-js-util-base')
+const Model = require('../models/model')
 
 const cloneSchema = (map, omittedFields = []) => {
   const arr = [...map].map(([key, schema]) => {
     const normalizedSchema = omit(schema, omittedFields)
-    const clonedSchema = cloneDeep(normalizedSchema)
+    const clonedSchema = {}
 
-    for (const [propName, value] of Object.entries(schema)) {
-      if (typeof value !== 'function') {
+    for (const [propName, value] of Object.entries(normalizedSchema)) {
+      if (
+        typeof value === 'function' ||
+        value instanceof Model
+      ) {
+        clonedSchema[propName] = value
+
         continue
       }
 
-      clonedSchema[propName] = value
+      clonedSchema[propName] = cloneDeep(value)
     }
 
     return [key, clonedSchema]
