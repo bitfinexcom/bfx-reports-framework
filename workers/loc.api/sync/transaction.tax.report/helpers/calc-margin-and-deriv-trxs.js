c4ef99c373340e1dccac665b2b51d7ce55eed4d2
Vladimir Voronkov <vsvoronkov@gmail.com>
2025-03-12 15:06:00 +0200

    Add ability to calc buy weighted price usd

--- workers/loc.api/sync/transaction.tax.report/helpers/calc-margin-and-deriv-trxs.js
+++ workers/loc.api/sync/transaction.tax.report/helpers/calc-margin-and-deriv-trxs.js
@@ -2,6 +2,63 @@
 
 const BigNumber = require('bignumber.js')
 const { setImmediate } = require('node:timers/promises')
+const splitSymbolPairs = require(
+  'bfx-report/workers/loc.api/helpers/split-symbol-pairs'
+)
+
+const {
+  CurrencyConversionError,
+  CurrencyPairSeparationError
+} = require('../../../errors')
+
+const calcTotalBuyAmount = (
+  mapOfLastProcessedTradesByPairs,
+  trade
+) => {
+  const prevTrade = mapOfLastProcessedTradesByPairs.get(trade.symbol)
+  const currAmount = trade.isBuyTrx
+    ? new BigNumber(trade.execAmount)
+    : new BigNumber(0)
+
+  if (!(prevTrade?.totalBuyAmount instanceof BigNumber)) {
+    return currAmount
+  }
+
+  return prevTrade.totalBuyAmount.plus(currAmount)
+}
+
+const calcTotalCostUsd = (
+  mapOfLastProcessedTradesByPairs,
+  trade
+) => {
+  const prevTrade = mapOfLastProcessedTradesByPairs.get(trade.symbol)
+  const currAmount = trade.isBuyTrx
+    ? new BigNumber(trade.execAmount)
+    : new BigNumber(0)
+  const currPriceUsd = trade.isBuyTrx
+    ? new BigNumber(trade.firstSymbPriceUsd)
+    : new BigNumber(0)
+
+  if (!(prevTrade?.totalCostUsd instanceof BigNumber)) {
+    return currAmount.times(currPriceUsd)
+  }
+
+  return prevTrade.totalCostUsd
+    .plus(currAmount.times(currPriceUsd))
+}
+
+const calcBuyWeightedPriceUsd = (trade) => {
+  if (
+    !(trade?.totalBuyAmount instanceof BigNumber) ||
+    !(trade?.totalCostUsd instanceof BigNumber) ||
+    trade.totalBuyAmount.eq(0) ||
+    trade.totalCostUsd.eq(0)
+  ) {
+    return new BigNumber(0)
+  }
+
+  return trade.totalCostUsd.div(trade.totalBuyAmount)
+}
 
 // TODO:
 module.exports = async (trades, opts) => {
@@ -9,18 +66,33 @@ module.exports = async (trades, opts) => {
     interrupter
   } = opts ?? {}
 
+  // TODO: mocked deriv
+  const res = [
+    {
+      asset: 'BTCF0:USTF0',
+      amount: 123,
+      mtsAcquired: null,
+      mtsSold: null,
+      proceeds: 200,
+      cost: 100,
+      gainOrLoss: 123,
+      type: 'DERIVATIVE'
+    }
+  ]
+
   if (
     !Array.isArray(trades) ||
     trades.length === 0
   ) {
-    return []
+    return res
   }
 
+  const mapOfLastProcessedTradesByPairs = new Map()
   let lastLoopUnlockMts = Date.now()
 
   for (const trade of trades) {
     if (interrupter?.hasInterrupted()) {
-      return []
+      return res
     }
 
     const currentLoopUnlockMts = Date.now()
@@ -35,18 +107,70 @@ module.exports = async (trades, opts) => {
 
       lastLoopUnlockMts = currentLoopUnlockMts
     }
+
+    trade.totalBuyAmount = trade
+      .totalBuyAmount ?? new BigNumber(0)
+    trade.totalCostUsd = trade
+      .totalCostUsd ?? new BigNumber(0)
+    trade.buyWeightedPriceUsd = trade
+      .buyWeightedPriceUsd ?? new BigNumber(0)
+
+    if (
+      !trade?.symbol ||
+      !Number.isFinite(trade?.execPrice) ||
+      !Number.isFinite(trade?.execAmount) ||
+      trade.execAmount === 0
+    ) {
+      continue
+    }
+
+    const [firstSymb, lastSymb] = (
+      trade?.firstSymb &&
+      trade?.lastSymb
+    )
+      ? [trade?.firstSymb, trade?.lastSymb]
+      : splitSymbolPairs(trade.symbol)
+    trade.firstSymb = firstSymb
+    trade.lastSymb = lastSymb
+
+    trade.isSaleTrx = trade.execAmount < 0
+    trade.isBuyTrx = trade.execAmount > 0
+
+    if (
+      !firstSymb ||
+      !lastSymb
+    ) {
+      throw new CurrencyPairSeparationError({
+        symbol: trade.symbol,
+        firstSymb,
+        lastSymb
+      })
+    }
+    if (!Number.isFinite(trade.firstSymbPriceUsd)) {
+      throw new CurrencyConversionError({
+        symbol: firstSymb,
+        priceUsd: trade.firstSymbPriceUsd
+      })
+    }
+    if (!Number.isFinite(trade.lastSymbPriceUsd)) {
+      throw new CurrencyConversionError({
+        symbol: lastSymb,
+        priceUsd: trade.lastSymbPriceUsd
+      })
+    }
+
+    trade.totalBuyAmount = calcTotalBuyAmount(
+      mapOfLastProcessedTradesByPairs,
+      trade
+    )
+    trade.totalCostUsd = calcTotalCostUsd(
+      mapOfLastProcessedTradesByPairs,
+      trade
+    )
+    trade.buyWeightedPriceUsd = calcBuyWeightedPriceUsd(trade)
+
+    mapOfLastProcessedTradesByPairs.set(trade.symbol, trade)
   }
 
-  return [
-    {
-      asset: 'BTCF0:USTF0',
-      amount: 123,
-      mtsAcquired: null,
-      mtsSold: null,
-      proceeds: 200,
-      cost: 100,
-      gainOrLoss: 123,
-      type: 'DERIVATIVE'
-    }
-  ]
+  return res
 }
