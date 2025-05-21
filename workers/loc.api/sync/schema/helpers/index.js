4ad20a1b3d22bb7dce3631dc80062cd461f0318f
Vladimir Voronkov <vsvoronkov@gmail.com>
2025-05-21 15:50:41 +0300

    Cleanup sync schema helpers

--- workers/loc.api/sync/schema/helpers/index.js
+++ workers/loc.api/sync/schema/helpers/index.js
@@ -1,32 +1,5 @@
 'use strict'
 
-const { omit, cloneDeep } = require('lib-js-util-base')
-const Model = require('../models/model')
-
-const cloneSchema = (map, omittedFields = []) => {
-  const arr = [...map].map(([key, schema]) => {
-    const normalizedSchema = omit(schema, omittedFields)
-    const clonedSchema = {}
-
-    for (const [propName, value] of Object.entries(normalizedSchema)) {
-      if (
-        typeof value === 'function' ||
-        value instanceof Model
-      ) {
-        clonedSchema[propName] = value
-
-        continue
-      }
-
-      clonedSchema[propName] = cloneDeep(value)
-    }
-
-    return [key, clonedSchema]
-  })
-
-  return new Map(arr)
-}
-
 const freezeAndSealObjectDeeply = (...args) => {
   for (const object of args) {
     if (
@@ -46,6 +19,5 @@ const freezeAndSealObjectDeeply = (...args) => {
 }
 
 module.exports = {
-  cloneSchema,
   freezeAndSealObjectDeeply
 }
