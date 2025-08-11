9a12b965d9b0d85e6f775534192d113b944d2561
Vladimir Voronkov <vsvoronkov@gmail.com>
2025-08-11 15:35:57 +0300

    Add data validator fwGetCandles req schema name

--- workers/loc.api/data-validator/schema.names.js
+++ workers/loc.api/data-validator/schema.names.js
@@ -3,6 +3,7 @@
 module.exports = {
   FW_DEFS: 'fwDefs',
 
+  FW_GET_CANDLES_REQ: 'fwGetCandlesReq',
   GET_BALANCE_HISTORY_REQ: 'getBalanceHistoryReq',
 
   GET_BALANCE_HISTORY_FILE_REQ: 'getBalanceHistoryFileReq'
