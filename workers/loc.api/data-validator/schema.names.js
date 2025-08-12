db9117d4c62b7743b6686d0908c90fafb03e7e13
Vladimir Voronkov <vsvoronkov@gmail.com>
2025-08-12 13:17:24 +0300

    Add data validator createSubAccount req schema name

--- workers/loc.api/data-validator/schema.names.js
+++ workers/loc.api/data-validator/schema.names.js
@@ -3,6 +3,7 @@
 module.exports = {
   FW_DEFS: 'fwDefs',
 
+  CREATE_SUB_ACCOUNT_REQ: 'createSubAccountReq',
   FW_GET_CANDLES_REQ: 'fwGetCandlesReq',
   GET_BALANCE_HISTORY_REQ: 'getBalanceHistoryReq',
 
