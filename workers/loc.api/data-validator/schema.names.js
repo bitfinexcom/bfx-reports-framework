eaa8f9d0c40f8112a8a63cb51abfa1bddf175bc2
Vladimir Voronkov <vsvoronkov@gmail.com>
2025-08-13 10:07:39 +0300

    Add data validator interruptOperations req schema name

--- workers/loc.api/data-validator/schema.names.js
+++ workers/loc.api/data-validator/schema.names.js
@@ -5,6 +5,7 @@ module.exports = {
 
   CREATE_SUB_ACCOUNT_REQ: 'createSubAccountReq',
   UPDATE_SUB_ACCOUNT_REQ: 'updateSubAccountReq',
+  INTERRUPT_OPERATIONS_REQ: 'interruptOperationsReq',
   FW_GET_CANDLES_REQ: 'fwGetCandlesReq',
   GET_BALANCE_HISTORY_REQ: 'getBalanceHistoryReq',
 
