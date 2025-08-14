d295ded16dd658f7a5020a353d221f86ac850d6a
Vladimir Voronkov <vsvoronkov@gmail.com>
2025-08-14 12:01:27 +0300

    Add data validator getWinLoss req schema name

--- workers/loc.api/data-validator/schema.names.js
+++ workers/loc.api/data-validator/schema.names.js
@@ -7,8 +7,10 @@ module.exports = {
   UPDATE_SUB_ACCOUNT_REQ: 'updateSubAccountReq',
   INTERRUPT_OPERATIONS_REQ: 'interruptOperationsReq',
   EDIT_ALL_PUBLIC_COLLS_CONFS_REQ: 'editAllPublicCollsConfsReq',
+
   FW_GET_CANDLES_REQ: 'fwGetCandlesReq',
   GET_BALANCE_HISTORY_REQ: 'getBalanceHistoryReq',
+  GET_WIN_LOSS_REQ: 'getWinLossReq',
 
   FW_GET_CANDLES_FILE_REQ: 'fwGetCandlesFileReq',
   GET_BALANCE_HISTORY_FILE_REQ: 'getBalanceHistoryFileReq'
