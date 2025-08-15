b2fa5e261861e6281b40df546859a9ece98d82d6
Vladimir Voronkov <vsvoronkov@gmail.com>
2025-08-15 12:08:17 +0300

    Add data validator getPositionsSnapshotFile req schema

---
+++ workers/loc.api/data-validator/schemas/getPositionsSnapshotFileReq.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const SCHEMA_IDS = require('../schema.ids')
+
+module.exports = {
+  $id: SCHEMA_IDS.GET_POSITIONS_SNAPSHOT_FILE_REQ,
+  type: 'object',
+  additionalProperties: false,
+  properties: {
+    end: {
+      $ref: 'defs#/definitions/end'
+    },
+
+    email: {
+      $ref: 'defs#/definitions/email'
+    },
+    milliseconds: {
+      $ref: 'defs#/definitions/milliseconds'
+    },
+    dateFormat: {
+      $ref: 'defs#/definitions/dateFormat'
+    },
+    language: {
+      $ref: 'defs#/definitions/language'
+    },
+    timezone: {
+      $ref: 'defs#/definitions/timezone'
+    },
+    isPDFRequired: {
+      $ref: 'defs#/definitions/isPDFRequired'
+    },
+    isSignatureRequired: {
+      $ref: 'defs#/definitions/isSignatureRequired'
+    }
+  }
+}
