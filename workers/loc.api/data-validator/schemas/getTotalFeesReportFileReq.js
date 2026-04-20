d0253d716c12ad159d199da2d9973547a12ab9e0
Vladimir Voronkov <vsvoronkov@gmail.com>
2026-04-20 14:47:03 +0300

    Remove isSignatureRequired param from validation

--- workers/loc.api/data-validator/schemas/getTotalFeesReportFileReq.js
+++ workers/loc.api/data-validator/schemas/getTotalFeesReportFileReq.js
@@ -43,9 +43,6 @@ module.exports = {
     },
     isPDFRequired: {
       $ref: 'defs#/definitions/isPDFRequired'
-    },
-    isSignatureRequired: {
-      $ref: 'defs#/definitions/isSignatureRequired'
     }
   }
 }
