33812d759d76e3e1fa06a105d4b4b0b69d8c1981
Vladimir Voronkov <vsvoronkov@gmail.com>
2026-04-23 13:13:01 +0300

    Move sum-all-objects-numbers util into separate file

--- workers/loc.api/helpers/utils/index.js
+++ workers/loc.api/helpers/utils/index.js
@@ -3,8 +3,10 @@
 const orderBy = require('./order-by')
 const pushLargeArr = require('./push-large-arr')
 const sumArrayVolumes = require('./sum-array-volumes')
+const sumAllObjectsNumbers = require('./sum-all-objects-numbers')
 
 module.exports = {
+  sumAllObjectsNumbers,
   sumArrayVolumes,
   pushLargeArr,
   orderBy
