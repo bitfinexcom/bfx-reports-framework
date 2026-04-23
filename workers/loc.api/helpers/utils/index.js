2bc4f69e5ded78690c9143a2ee2cbbef33f04a75
Vladimir Voronkov <vsvoronkov@gmail.com>
2026-04-23 13:07:23 +0300

    Move sum-array-volumes util into separate file

--- workers/loc.api/helpers/utils/index.js
+++ workers/loc.api/helpers/utils/index.js
@@ -2,8 +2,10 @@
 
 const orderBy = require('./order-by')
 const pushLargeArr = require('./push-large-arr')
+const sumArrayVolumes = require('./sum-array-volumes')
 
 module.exports = {
+  sumArrayVolumes,
   pushLargeArr,
   orderBy
 }
