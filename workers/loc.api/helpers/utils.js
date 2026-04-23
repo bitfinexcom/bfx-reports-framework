4f56c528aeaafbc5b4a43d12596fcf4badf3cdc8
Vladimir Voronkov <vsvoronkov@gmail.com>
2026-04-23 12:24:42 +0300

    Implement orderBy util to remove lodash totally

--- workers/loc.api/helpers/utils.js
+++ workers/loc.api/helpers/utils.js
@@ -272,6 +272,58 @@ const pushLargeArr = (dest, src) => {
   }
 }
 
+const _getValue = (obj, path) => {
+  if (typeof path !== 'string') {
+    return obj?.[path]
+  }
+
+  return path.split('.').reduce((accum, key) => accum?.[key], obj)
+}
+
+const orderBy = (collection, iteratees = [], orders = []) => {
+  // It's able to consider iterable objects as well
+  const copiedColl = [...collection]
+
+  return copiedColl.sort((a, b) => {
+    for (const [i, iteratee] of iteratees.entries()) {
+      const direction = orders[i] === 'desc' ? -1 : 1
+
+      const valA = typeof iteratee === 'function'
+        ? iteratee(a)
+        : _getValue(a, iteratee)
+      const valB = typeof iteratee === 'function'
+        ? iteratee(b)
+        : _getValue(b, iteratee)
+
+      if (valA === valB) {
+        continue
+      }
+
+      if (
+        valA === undefined ||
+        valA === null
+      ) {
+        return 1
+      }
+      if (
+        valB === undefined ||
+        valB === null
+      ) {
+        return -1
+      }
+
+      if (valA > valB) {
+        return direction
+      }
+      if (valA < valB) {
+        return -direction
+      }
+    }
+
+    return 0
+  })
+}
+
 module.exports = {
   checkParamsAuth,
   tryParseJSON,
@@ -283,5 +335,6 @@ module.exports = {
   sumAllObjectsNumbers,
   pickAllLowerObjectsNumbers,
   sumArrayVolumes,
-  pushLargeArr
+  pushLargeArr,
+  orderBy
 }
