3c055249bfd1ae1ccbe3e7a728889040fe7b81f7
Vladimir Voronkov <vsvoronkov@gmail.com>
2026-04-24 11:43:30 +0300

    Move sum-objects-numbers util into separate file

--- workers/loc.api/helpers/utils/index.js
+++ workers/loc.api/helpers/utils/index.js
@@ -1,15 +1,17 @@
 'use strict'
 
-const orderBy = require('./order-by')
-const pushLargeArr = require('./push-large-arr')
-const sumArrayVolumes = require('./sum-array-volumes')
-const sumAllObjectsNumbers = require('./sum-all-objects-numbers')
-const pickAllLowerObjectsNumbers = require('./pick-all-lower-objects-numbers')
 const pickLowerObjectsNumbers = require('./pick-lower-objects-numbers')
+const pickAllLowerObjectsNumbers = require('./pick-all-lower-objects-numbers')
+const sumObjectsNumbers = require('./sum-objects-numbers')
+const sumAllObjectsNumbers = require('./sum-all-objects-numbers')
+const sumArrayVolumes = require('./sum-array-volumes')
+const pushLargeArr = require('./push-large-arr')
+const orderBy = require('./order-by')
 
 module.exports = {
   pickLowerObjectsNumbers,
   pickAllLowerObjectsNumbers,
+  sumObjectsNumbers,
   sumAllObjectsNumbers,
   sumArrayVolumes,
   pushLargeArr,
