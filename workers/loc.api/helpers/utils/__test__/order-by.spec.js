7364970aed1d610bc8b08c5c3712255e2a6860f7
Vladimir Voronkov <vsvoronkov@gmail.com>
2026-04-27 11:00:18 +0300

    Add asc order test case for util

--- workers/loc.api/helpers/utils/__test__/order-by.spec.js
+++ workers/loc.api/helpers/utils/__test__/order-by.spec.js
@@ -22,4 +22,22 @@ describe('orderBy util', () => {
       { name: 'aaa' }
     ])
   })
+
+  it('Order array of objects by name prop in asc', () => {
+    const mockedArr = [
+      { name: 'aaa' },
+      { name: 'www' },
+      { name: 'bbb' },
+      { name: 'yyy' }
+    ]
+
+    const orderedArr = orderBy(mockedArr, ['name'], ['asc'])
+
+    assert.deepStrictEqual(orderedArr, [
+      { name: 'aaa' },
+      { name: 'bbb' },
+      { name: 'www' },
+      { name: 'yyy' }
+    ])
+  })
 })
