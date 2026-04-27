b89bffdb88d20b5461029a01065377f1b4c31cbd
Vladimir Voronkov <vsvoronkov@gmail.com>
2026-04-27 10:56:46 +0300

    Add desc order test case for util

---
+++ workers/loc.api/helpers/utils/__test__/order-by.spec.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const { assert } = require('chai')
+
+const orderBy = require('../order-by')
+
+describe('orderBy util', () => {
+  it('Order array of objects by name prop in desc', () => {
+    const mockedArr = [
+      { name: 'aaa' },
+      { name: 'www' },
+      { name: 'bbb' },
+      { name: 'yyy' }
+    ]
+
+    const orderedArr = orderBy(mockedArr, ['name'], ['desc'])
+
+    assert.deepStrictEqual(orderedArr, [
+      { name: 'yyy' },
+      { name: 'www' },
+      { name: 'bbb' },
+      { name: 'aaa' }
+    ])
+  })
+})
