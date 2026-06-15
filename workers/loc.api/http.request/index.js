6fc35871d3c7baae91f8d470b845abb69a92751a
Vladimir Voronkov <vsvoronkov@gmail.com>
2026-06-15 11:52:07 +0300

    Add captcha-required endpoint support

--- workers/loc.api/http.request/index.js
+++ workers/loc.api/http.request/index.js
@@ -61,6 +61,11 @@ class ExpandedRESTv2 extends RESTv2 {
     return makeRequestToBFX(() => this
       ._makePublicPostRequest('/int/captcha/providers', body))
   }
+
+  async isCaptchaRequired (body) {
+    return makeRequestToBFX(() => this
+      ._makePublicPostRequest('/int/captcha/required', body))
+  }
 }
 
 decorateInjectable(HTTPRequest, depsTypes)
